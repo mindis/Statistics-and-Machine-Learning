41665340a75ae55eb0afe6e1eb9e3d457dcb1967
you-chan2 <verticalbird2007@yahoo.co.jp>
2017-05-11 17:33:59 +0900

    【統計モデル】混合ロジスティック回帰モデル　

--- mixture_logistic_regression_model.R
+++ mixture_logistic_regression_model.R
@@ -19,7 +19,7 @@ seg.z <- rep(1:4, rep(n, k))
 
 
 ##説明変数の設定
-X1 <- matrix(runif(N*(col-4), -1, 1), N, (col-4))
+X1 <- matrix(runif(N*(col-4), 0, 1), N, (col-4))
 X2 <- matrix(0, N, (col-ncol(X1)))
 for(i in 1:(col-ncol(X1))){
   bin <- rbinom(N, 1, runif(1, 0.2, 0.7))
@@ -28,14 +28,14 @@ for(i in 1:(col-ncol(X1))){
 X <- cbind(X1, X2)
 
 ##回帰係数の発生
-b1 <- c(runif(ncol(X1), 0, 1.0), runif(ncol(X2), -1.5, 1.5))   #回帰係数1
-b2 <- c(runif(ncol(X1), 0, 1.9), runif(ncol(X2), -1.5, 1.5))   #回帰係数1
-b3 <- c(runif(ncol(X1), 0, 2.3), runif(ncol(X2), -1.5, 1.5))   #回帰係数1
-b4 <- c(runif(ncol(X1), 0, 2.5), runif(ncol(X2), -0.5, 1.5))   #回帰係数1
+b1 <- c(rnorm(ncol(X1), 0, 0.7), runif(ncol(X2), -1.0, 1.0))   #回帰係数1
+b2 <- c(rnorm(ncol(X1), 0, 1.2), runif(ncol(X2), -1.2, 1.4))   #回帰係数1
+b3 <- c(rnorm(ncol(X1), 0, 0.4), runif(ncol(X2), -0.7, 0.8))   #回帰係数1
+b4 <- c(rnorm(ncol(X1), 0, 1.6), runif(ncol(X2), -0.5, 0.3))   #回帰係数1
 b1
-b01 <- -0.3   #切片1
+b01 <- -0.4   #切片1
 b02 <- 0.8   #切片2
-b03 <- 0.4   #切片3
+b03 <- 1.3   #切片3
 b04 <- -0.6   #切片4
 betat <- c(b01, b02, b03, b04, b1, b2, b3, b4)
 
@@ -69,31 +69,20 @@ for(i in 1:n){
 y <- c(y1, y2, y3, y4)
 YX <- data.frame(seg=seg.z, y, X)   #すべてのデータを結合
 
-logit11 <- b01 + X %*% b1
-logit12 <- b02 + X %*% b2
-logit13 <- b03 + X %*% b3
-logit14 <- b04 + X %*% b4
-p11 <- exp(logit11) / (1+exp(logit11))
-p12 <- exp(logit12) / (1+exp(logit12))
-p13 <- exp(logit13) / (1+exp(logit13))
-p14 <- exp(logit14) / (1+exp(logit14))
-
-round(data.frame(y, p11, p12, p13, p14), 3)
-
 table(y)   #全体でのyの単純集計
 table(YX$seg, YX$y)   #セグメント別でのクロス集計
 round(table(YX$seg, YX$y) / rowSums(table(YX$seg, YX$y)), 3)   #セグメント別での比率クロス集計
 
 #確率分布をプロット
-hist(p1, breaks=20, col="#0000ff40", xlim=c(0, 1.0), ylim=c(0, 320), border = "#0000ff",
-     xlab="rate", main="ディクレリ多項分布")
-hist(p2, breaks=20, xlim=c(0, 1), ylim=c(0, 320), col="#ff00ff40", border = "#ff00ff",
-     xlab="rate", main="ディクレリ多項分布", add=T)
-hist(p3, breaks=20, xlim=c(0, 1), ylim=c(0, 320), col="#a5f0ff40", border = "#ff00ff",
-     xlab="rate", main="ディクレリ多項分布", add=T)
-hist(p4, breaks=20, xlim=c(0, 1), ylim=c(0, 320), col="#5f90f055", border = "#ff00ff",
-     xlab="rate", main="ディクレリ多項分布", add=T)
+#セグメントでの分布
+hist(p1, col="#0000ff40", xlim=c(0, 1.0), border = "#0000ff", xlab="rate", main="確率の分布")
+hist(p2, xlim=c(0, 1), col="#ff00ff40", border = "#ff00ff", xlab="rate", main="確率の分布")
+hist(p3, xlim=c(0, 1), col="#a5f0ff40", border = "#ff00ff", xlab="rate", main="確率の分布")
+hist(p4, xlim=c(0, 1), col="#5f90f055", border = "#ff00ff", xlab="rate", main="確率の分布")
 
+#全体での分布
+hist(c(p1, p2, p3, p4), breaks=20, xlim=c(0, 1), col="#0000ff40", border = "#ff00ff",
+     xlab="rate", main="確率の分布")
 
 ####EMアルゴリズムによる混合ロジスティック回帰モデルの推定####
 ##完全データでの混合ロジスティック回帰モデルの対数尤度
@@ -101,7 +90,7 @@ fr <- function(b, ID, hh, X, Y, k, col, zpt){
   beta0 <- b[1:k]
   beta1 <- b[(k+1):(k+k*col)]
   betaM <- matrix(beta1, k, col, byrow=T)
- 
+  
   #尤度を定義して和を取る
   #セグメントごとのロジットリンク関数を計算
   logit1 <- beta0[1] + X %*% betaM[1, ]
@@ -109,7 +98,7 @@ fr <- function(b, ID, hh, X, Y, k, col, zpt){
   logit3 <- beta0[3] + X %*% betaM[3, ]
   logit4 <- beta0[4] + X %*% betaM[4, ]
   logit <- cbind(logit1, logit2, logit3, logit4)
-
+  
   #対数尤度を計算
   Ym <- matrix(y, N, k)   #反応変数yをセグメントを列とした行列にする
   P <- exp(logit)/(1+(exp(logit)))   #確率の計算
@@ -132,7 +121,7 @@ obsll <- function(x, ID, hh, X, r, y, N, k, col){
   logit3 <- beta0[3] + X %*% betaM[3, ]
   logit4 <- beta0[4] + X %*% betaM[4, ]
   logit <- cbind(logit1, logit2, logit3, logit4)
-
+  
   #尤度と対数尤度を計算
   P <- exp(logit)/(1+(exp(logit)))   #確率の計算
   Ym <- matrix(y, N, k)   #反応変数yをセグメントを列とした行列にする
@@ -168,12 +157,14 @@ logi.f <- glm(y ~ X, family="binomial")
 coef.f <- logi.f$coefficients
 
 betaf0 <- coef.f[1] + runif(k, -1.0, 1.0)
-betaf1 <- rep(coef.f[2:length(fc)], 4) + runif(k*col, -0.5, 0.5)
+betaf1 <- rep(coef.f[2:length(coef.f)], 4) + runif(k*col, -0.5, 0.5)
 beta <- as.numeric(c(betaf0, betaf1))
 r <- c(0.2, 0.2, 0.3, 0.3)   #混合率の初期値
 
 #観測データの尤度と潜在変数zの初期値
 obsllz <- obsll(x=beta, ID=ID, hh=hh, X=X, y=y, r=r, N=N, k=k, col=col)
+z <- obsllz$z1
+LL1 <- obsllz$LLobz
 
 dl <- 100   #EMステップでの対数尤度の初期値の設定
 tol <- 1
@@ -181,6 +172,7 @@ tol <- 1
 ##EMアルゴリズムによる推定
 while(abs(dl) >= tol){   #dlがtol以上なら繰り返す
   ##完全データでの混合ロジスティック回帰モデルの推定(Mステップ)
+  zpt <- matrix(0, N, k)
   for(i in 1:N){
     zpt[i, ] <- z[ID[i], ]
   }
@@ -188,7 +180,7 @@ while(abs(dl) >= tol){   #dlがtol以上なら繰り返す
   #準ニュートン法で完全データを最適化
   res <- optim(beta, fr, gr=NULL, hh=hh, X=X, Y=y, k=k, col=col, zpt=zpt, method="BFGS", 
                hessian=FALSE, control=list(fnscale=-1))
-    
+  
   beta <- as.numeric(res$par)   #推定されたパラメータ
   r <- apply(z, 2, sum) / hh   #混合率の計算
   
@@ -202,7 +194,20 @@ while(abs(dl) >= tol){   #dlがtol以上なら繰り返す
   LL1 <- LL
   print(LL)
 }
-r
-round(z, 3)
-round(res$par, 3)
-round(betat, 3)
+
+####推定結果と要約####
+##混合率と潜在変数z
+round(r, 3)   #混合率の推定値
+round(z, 3)   #潜在変数zの推定値
+
+#3真の回帰係数と推定された回帰係数の比較
+beta <- res$par
+round(beta[1:k], 2)   #切片の推定値
+round(betat[1:k], 2)   #真の切片
+round(matrix(beta[(k+1):length(beta)], k, col), 2)   #回帰係数の推定値
+round(matrix(betat[(k+1):length(betat)], k, col), 2)   #真の回帰係数
+
+##GLMと混合ロジスティック回帰モデルのAICの比較
+round(AIC <- -2*LL + 2*(length(res$par)+length(res$par)), 3)   #AIC
+round(logi.f$aic, 3)   #GLMの対数尤度
+
