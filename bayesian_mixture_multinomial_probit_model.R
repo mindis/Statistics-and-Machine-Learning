46df5bbfc094743c3b852c7d7089d7f23e6a50e4
you-chan2 <verticalbird2007@yahoo.co.jp>
2017-05-23 08:51:36 +0900

    【統計モデル】ベイジアン混合多項プロビットモデル

--- bayesian_mixture_multinomial_probit_model.R
+++ bayesian_mixture_multinomial_probit_model.R
@@ -127,6 +127,10 @@ for(i in 1:seg){
   Cov[[i]] <- Sigma$covariance
 }
 
+
+##セグメントの設定
+z.seg <- rep(1:seg, rep(hh, seg))
+
 ##パラメータの設定
 beta1 <- runif(seg, -9.0, -5.0)   #価格のパラメータ
 beta2 <- runif(seg, 6.2, 9.2)   #割引率のパラメータ
@@ -180,8 +184,88 @@ for(i in 1:H){
   BUY[i, Y[i]] <- 1
 }
 
+
+
+
 table(Y)   #選択ブランドの集計
 round(data.frame(Y, U=U), 1)   #効用と選択ブランドを比較
 
-?rmnpGibbs
-?bayesm
+
+####マルコフ連鎖モンテカルロ法で多項プロビットモデルを推定####
+####MCMC推定のための推定準備####
+
+##切断正規分布の乱数を発生させる関数
+rtnorm <- function(mu, sigma, a, b){
+  FA <- pnorm(a, mu, sigma)
+  FB <- pnorm(b, mu, sigma)
+  return(qnorm(runif(length(mu))*(FB-FA)+FA, mu, sigma))
+}
+
+##MCMCアルゴリズムの設定
+R <- 20000
+keep <- 2
+
+##回帰モデルを推定するために説明変数をベクトル形式に変更設定
+#切片の設定
+p <- c(1, rep(0, choise-1))
+bp <- matrix(p, nrow=H*choise, ncol=choise-1, byrow=T)
+BP <- subset(bp, rowSums(bp) > 0)
+
+#説明変数の設定
+PRICE.v <- as.numeric(t(PRICE.r))
+DISC.v <- as.numeric(t(DISC.r))
+DISP.v <- as.numeric(t(DISP.r))
+CAMP.v <- as.numeric(t(CAMP.r))
+
+X <- data.frame(BP=BP, PRICE.v, DISC.v, DISP.v, CAMP.v)   #データの結合
+XM <- as.matrix(X)   #データ形式を行列に変換
+
+##事前分布の設定
+a <- rep(10, seg)   #ディクレリ事前分布
+
+
+
+
+set.seed(66)
+p=3
+n=500
+beta=c(-1,1,1,2)
+sigma=matrix(c(1,.5,.5,1),ncol=2)
+k=length(beta)
+X1=matrix(runif(n*p,min=0,max=2),ncol=p); X2=matrix(runif(n*p,min=0,max=2),ncol=p)
+X=createX(p,na=2,nd=NULL,Xa=cbind(X1,X2),Xd=NULL,DIFF=TRUE,base=p)
+
+simmnp= function(X,p,n,beta,sigma) {
+  indmax=function(x) {which(max(x)==x)}
+  Xbeta=X%*%beta
+  w=as.vector(crossprod(chol(sigma),matrix(rnorm((p-1)*n),ncol=n)))+ Xbeta
+  w
+  w=matrix(w,ncol=(p-1),byrow=TRUE)
+  maxw=apply(w,1,max)
+  y=apply(w,1,indmax)
+  y=ifelse(maxw < 0,p,y)
+  return(list(y=y,X=X,beta=beta,sigma=sigma))
+}
+(crossprod(chol(sigma),matrix(rnorm((p-1)*n),ncol=n)))
+Xbeta
+matrix(rnorm((p-1)*n),ncol=n)
+chol(sigma)
+
+simout=simmnp(X,p,500,beta,Sigma)
+
+Data1=list(p=p,y=simout$y,X=simout$X)
+Mcmc1=list(R=20000,keep=1)
+
+out=rmnpGibbs(Data=Data1,Mcmc=Mcmc1)
+round(colMeans(out$betadraw[10000:20000, ]), 2)
+beta
+
+cat(" Summary of Betadraws ",fill=TRUE)
+betatilde=out$betadraw/sqrt(out$sigmadraw[,1])
+attributes(betatilde)$class="bayesm.mat"
+summary(betatilde,tvalues=beta)
+
+cat(" Summary of Sigmadraws ",fill=TRUE)
+sigmadraw=out$sigmadraw/out$sigmadraw[,1]
+attributes(sigmadraw)$class="bayesm.var"
+summary(sigmadraw,tvalues=as.vector(Sigma[upper.tri(Sigma,diag=TRUE)]))
