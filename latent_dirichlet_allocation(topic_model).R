a8a08397013efa3aee09c86bfde1d5f301e65aa1
you-chan2 <verticalbird2007@yahoo.co.jp>
2017-06-06 08:56:42 +0900

    【統計モデル】トピックモデル(Latent Dirichlet Allocation Model)

--- latent_dirichlet_allocation(topic_model).R
+++ latent_dirichlet_allocation(topic_model).R
@@ -1,4 +1,4 @@
-#####トピックモデル#####
+#####トピックモデル(Latent Dirichlet Allocation Model)#####
 library(MASS)
 library(lda)
 library(RMeCab)
@@ -11,13 +11,13 @@ library(ggplot2)
 ####データの発生####
 #set.seed(423943)
 #データの設定
-k <- 5   #トピック数
+k <- 4   #トピック数
 d <- 200   #文書数
-v <- 100   #語彙数
-w <- 300   #1文書あたりの単語数
+v <- 50   #語彙数
+w <- 100   #1文書あたりの単語数 
 
 #パラメータの設定
-alpha0 <- round(runif(k, 1.5, 2.5), 3)   #文書のディレクリ事前分布のパラメータ
+alpha0 <- runif(k, 0.1, 1.25)   #文書のディレクリ事前分布のパラメータ
 alpha1 <- rep(0.5, v)   #単語のディレクリ事前分布のパラメータ
 
 #ディレクリ乱数の発生
@@ -27,6 +27,7 @@ phi0 <- rdirichlet(k, alpha1)   #単語のトピック分布をディレクリ乱数から発生
 #多項分布の乱数からデータを発生
 WX <- matrix(0, d, v)
 ZS <- list()
+
 for(i in 1:d){
   z <- t(rmultinom(w, 1, theta0[i, ]))   #文書のトピック分布を発生
   
@@ -39,15 +40,15 @@ for(i in 1:d){
   ZS[[i]] <- zdn[, 1]
   print(i)
 }
-barplot(colSums(z), names.arg=c("seg1", "seg2", "seg3", "seg4", "seg5"))
+barplot(colSums(z), names.arg=c("seg1", "seg2", "seg3", "seg4"))
 round(colSums(WX)/sum(WX), 3)   #単語の出現頻度
 
 
 ####マルコフ連鎖モンテカルロ法でトピックモデルを推定####
 ##マルコフ連鎖モンテカルロ法の設定
 #アルゴリズムの設定
-R <- 20000   #サンプリング回数
-keep <- 4   #4回に1回の割合でサンプリング結果を利用
+R <- 5000   #サンプリング回数
+keep <- 2   #4回に1回の割合でサンプリング結果を利用
 iter <- 0
 
 #ハイパーパラメータの事前分布の設定
@@ -63,23 +64,44 @@ phi <- phi.ini/rowSums(phi.ini)   #単語トピックのパラメータの初期値
 #パラメータの格納用配列
 THETA <- array(0, dim=c(d, k, R/keep))
 PHI <- array(0, dim=c(k, v, R/keep))
-W.SEG <- matrix(0, nrow=d*v, ncol=R/keep)
+W.SEG <- matrix(0, nrow=d*w, ncol=R/(keep*2))
 
 
 ####ギブスサンプリングでトピックモデルを推定####
 id1 <- rep(1:d, v)
 id2 <- rep(1:v, rep(d, v))
 
+##単語頻度行列をベクトルに変換
+d.vec <- c() 
+v.vec <- c()
+for(i in 1:v){
+  for(j in 1:d){
+    d.vec <- c(d.vec, rep(j, WX[j, i]))
+    v.vec <- c(v.vec, rep(i, WX[j, i]))
+  }
+}
+index.vec <- cbind(v.vec, d.vec)
+id.w <- cbind(id2, id1)
+
+
+#抽出する潜在確率の行を取得
+index.z <- apply(index.vec, 1, function(x) subset(1:nrow(id.w), id.w[, 1]==x[1] & id.w[, 2]==x[2]))
+
+#トピックzをサンプリングするための配列
+theta.seg <- array(0, dim=c(d, v, k))
+phi.seg <- array(0, dim=c(d, v, k))
+burden.seg <- array(0, dim=c(d, v, k))
+zpt <- array(0, dim=c(d, v, k))
+
+#パラメータをサンプリングするための配列
+dir.theta <- matrix(0, nrow=d, ncol=k) 
+dir.phi <- matrix(0, nrow=k, ncol=v)
+
+####ギブスサンプリング####
 ##単語のトピックzをサンプリング
 for(rp in 1:R){
   
-  #トピックzをサンプリングするための配列
-  theta.seg <- array(0, dim=c(d, v, k))
-  phi.seg <- array(0, dim=c(d, v, k))
-  burden.seg <- array(0, dim=c(d, v, k))
   burden.sum <- matrix(0, nrow=d, ncol=v) 
-  zpt <- array(0, dim=c(d, v, k))
-  
   #潜在確率のパラメータを計算
   for(i in 1:k){
     theta.seg[, , i] <- matrix(theta[, i], nrow=d, ncol=v)
@@ -88,31 +110,30 @@ for(rp in 1:R){
     burden.sum <- burden.sum + burden.seg[, , i]
   }
 
-  
   #潜在確率を計算
   for(i in 1:k){
     zpt[, , i] <- burden.seg[, , i] / burden.sum
   }
-  ZP <- matrix(zpt, nrow=v*d, ncol=k)   #潜在確率を単語順に配列
- 
-  #単語ごとにトピックzをサンプリング
-  Z <- t(apply(ZP, 1, function(x) rmultinom(1, 1, x)))
   
+  ZP <- matrix(zpt, nrow=v*d, ncol=k)   #潜在確率を単語順に配列
+  ZW <- ZP[index.z, ]
+  
+  #単語ごとにトピックzをサンプリング
+  Z <- t(apply(ZW, 1, function(x) rmultinom(1, 1, x)))
   
   ##文書トピック分布thetaをサンプリング
   #ディレクリ分布のパラメータを決定
-  dir.theta <- matrix(0, nrow=d, ncol=k) 
   for(i in 1:d){
-   dir.theta[i, ] <- colSums(Z[id1==i, ]) + alpha 
+   dir.theta[i, ] <- colSums(Z[d.vec==i, ]) + alpha 
   }
   theta <- t(apply(dir.theta, 1, function(x) rdirichlet(1, x)))   #ディレクリ分布からthetaをサンプリング
-  
+
   ##単語トピック分布phiをサンプリング
   #ディレクリ分布のパラメータを決定
-  dir.phi <- matrix(0, nrow=k, ncol=v)
   for(i in 1:v){
-   dir.phi[, i] <- colSums(matrix(WX[, i], nrow=d, ncol=k) * Z[id2==i, ]) + beta[i]
+   dir.phi[, i] <- colSums(matrix(WX[, i], nrow=length(v.vec[v.vec==i]), ncol=k) * Z[v.vec==i, ]) + beta[i]
   }
+  
   phi <- t(apply(dir.phi, 1, function(x) rdirichlet(1, x)))   #ディレクリ分布からphiをサンプリング
   
   ##サンプリング結果の保存
@@ -121,38 +142,33 @@ for(rp in 1:R){
     mkeep <- rp/keep
     THETA[, , mkeep] <- theta
     PHI[, , mkeep] <- phi
-    W.SEG[, mkeep] <- Z %*% 1:5
+  }
+  if(rp%%(keep*2)==0){
+    W.SEG[, mkeep/2] <- Z %*% 1:k
     print(rp)
   }
 }
 
-Z.cnt <- t(apply(W.SEG[, 2500:500], 1, function(x) xtabs(~ x)))
 
-ZS.cnt
-for(i in 1:v){
-  for(j in 1:d){
-    ZS[[j]][i]
-  }
-}
-ZS[[1]]
+####推定結果と適合度####
+burnin <- 1000
+Rkeep <- R/keep
+
+##サンプリング結果のプロット
+matplot(t(THETA[1:3, 1, ]), type="l", lty=1, ylab="value")
+matplot(t(THETA[1:3, 2, ]), type="l", lty=1, ylab="value")
+matplot(t(PHI[1, 1:3, ]), type="l", lty=1, ylab="value")
+matplot(t(PHI[2, 1:3, ]), type="l", lty=1, ylab="value")
+
+#thetaの事後平均と真のthetaの比較
+round(THETA1 <- cbind(theta0, rowMeans(THETA[, 1, burnin:Rkeep]), rowMeans(THETA[, 2, burnin:Rkeep]), 
+      rowMeans(THETA[, 3, burnin:Rkeep]), rowMeans(THETA[, 4, burnin:Rkeep])), 3)
+
+#phiの事後平均と真のphiの比較
+round(PHI1 <- rbind(phi0, rowMeans(PHI[1, , burnin:Rkeep]), rowMeans(PHI[2, , burnin:Rkeep]),
+      rowMeans(PHI[3, , burnin:Rkeep]), rowMeans(PHI[4, , burnin:Rkeep])), 3)
+
+#トピックの事後分布
+segment <- apply(W.SEG[, (burnin/2):(Rkeep/2)], 1, table)
 
 
-colMeans(rdirichlet(1000, dir.phi[1, ]))
-
-
-cbind(round(theta0, 3), round(THETA[, , 2000], 3))
-
-round(rowMeans(THETA[1:100, 1, 1000:2000]), 3)
-round(theta0[1:100, 1], 3)
-
-plot(THETA[1, 1, 1:2000], type="l")
-
-t(THETA[1, , 1000:2000])
-
-rowMeans(THETA[1:10, 1, 1000:2000])
-theta[1:10, 1]
-
-colSums(matrix(WX[, i], nrow=d, ncol=k) * Z[id2==i, ])
-
-rowMeans(THETA[1:100, 1, 2500:5000])
-theta0[1:100, 3]
