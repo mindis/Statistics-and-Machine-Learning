3f10e9ebb0d4eb79dcd2bbf85cb4a9c681277e0a
you-chan2 <verticalbird2007@yahoo.co.jp>
2017-06-30 08:56:33 +0900

    【統計モデル】条件付き多変量回帰モデル

--- Conditionally_multivariate_regression_model.R
+++ Conditionally_multivariate_regression_model.R
@@ -99,8 +99,8 @@ SCALE <- rep(scale, rep(pt, hh))
 
 
 ##分散共分散行列の設定
-corM <- corrM(col=choise, lower=-0.5, upper=0.60)   #相関行列を作成
-Sigma <- covmatrix(col=choise, corM=corM, lower=0.8, upper=1.25)   #分散共分散行列
+corM <- corrM(col=choise, lower=-0.5, upper=0.75)   #相関行列を作成
+Sigma <- covmatrix(col=choise, corM=corM, lower=0.15, upper=0.2)   #分散共分散行列
 Cov <- Sigma$covariance
 
 
@@ -111,38 +111,68 @@ beta3 <- 0.5   #特別陳列のパラメータ
 beta4 <- 0.44   #キャンペーンのパラメータ
 beta5 <- c(0.08, 0.12, -0.08, 0.06, -0.04)   #店舗規模のパラメータ
 beta0 <- c(3.1, 2.7, 4.2, 3.6, 4.5)   #ブランド1～4の相対ベース販売力
+betat <- c(beta0, beta1, beta2, beta3, beta4, beta5)
 
 
 ##レジ延べ通過人数1000人あたりの売上数
 BUY.mean <- matrix(0, nrow=hhpt, ncol=choise)
 for(i in 1:choise){
-  BUY.ind <- beta0 + beta1*PRICE[, i] + beta2*DISC[, i] + beta3*DISP[, i] + beta4*CAMP[, i] + beta5[i]*SCALE 
-  BUY.mean[, i] <- exp(BUY.ind)
+  BUY.ind <- beta0[i] + beta1*PRICE[, i] + beta2*DISC[, i] + beta3*DISP[, i] + beta4*CAMP[, i] + beta5[i]*SCALE 
+  BUY.mean[, i] <- BUY.ind
 }
-BUY <- BUY.mean + exp(mvrnorm(hhpt, rep(0, choise), Cov))
+BUY <- exp(BUY.mean + mvrnorm(hhpt, rep(0, choise), Cov))
 summary(BUY)
 
 ####条件付き多変量回帰モデルを推定####
 ##データフォーマットを変更
 BUY.vec <- as.numeric(t(BUY))
 PRICE.vec <- as.numeric(t(PRICE))
-PRICE.vec <- as.numeric(t(DISC))
-PRICE.vec <- as.numeric(t(DISP))
-PRICE.vec <- as.numeric(t(SCALE))
+DISC.vec <- as.numeric(t(DISC))
+DISP.vec <- as.numeric(t(DISP))
+CAMP.vec <- as.numeric(t(CAMP))
+SCALE.vec <- as.numeric(t(SCALE))
 
 #切片の設定
-BP <- matrix(0, nrow=hhpt*choise, ncol=choise)
+BP.vec <- matrix(0, nrow=hhpt*choise, ncol=choise)
 for(i in 1:hhpt){
   r <- ((i-1)*choise+1):((i-1)*choise+choise)
-  BP[r, ] <- diag(choise) 
+  BP.vec[r, ] <- diag(choise) 
 }
 
 #店舗スケールの設定
 SCALE.vec <- matrix(0, nrow=hhpt*choise, ncol=choise)
-for(i in 1:hh){
-  r <- ((i-1)*(choise*pt)+1):((i-1)*(choise*pt)+choise*pt)
-  SCALE.vec <- S
+for(i in 1:hhpt){
+  r <- ((i-1)*choise+1):((i-1)*choise+choise)
+  SCALE.vec[r, ] <- diag(SCALE[i], choise)
 }
 
+##IDの設定
+id.v <- rep(1:hh, rep(choise*pt, hh))
+pd <- rep(1:choise, hhpt)
+t.vec <- rep(rep(1:pt, rep(choise, pt)), hh)
+ID.vec <- data.frame(no=1:hhpt*choise, id=id.v, t=t.vec, pd=pd)
 
+##データの結合
+YX.vec <- cbind(ID.vec, BUY.vec, PRICE.vec, DISC.vec, DISP.vec, CAMP.vec, SCALE.vec)
+X.vec <- cbind(BP.vec, PRICE.vec, DISC.vec, DISP.vec, CAMP.vec, SCALE.vec)
+round(X.vec, 3)
 
+X.vec
+####最小二乗法で条件付き多変量回帰モデルを推定####
+##最小二乗法でパラメータを推定
+round(beta <- as.numeric(solve(t(X.vec) %*% X.vec) %*% t(X.vec) %*% log(BUY.vec)), 2)
+round(betat, 2)   #真のパラメータ
+
+##分散共分散行列を推定
+#予測値と誤差を計算
+BUY.pred <- matrix(X.vec %*% beta, nrow=hhpt, ncol=choise, byrow=T)   #購入数の予測値
+error <- (log(BUY) - BUY.pred)   
+
+#分散共分散行列と相関行列を計算
+round(Cov_hat <- 1/hhpt * t(error) %*% error, 3)   #推定された分散共分散行列
+round(Cov, 3)   #真の分散共分散行列
+round(Cor_hat <- cov2cor(Cov_hat), 3)   #推定された相関行列
+round(cov2cor(Cov), 3)   #真の相関行列
+
+#予測値と真の平均構造および真の購入数
+round(BUY.comp <- data.frame(pred=exp(BUY.pred), mean=exp(BUY.mean), buy=BUY), 0)
