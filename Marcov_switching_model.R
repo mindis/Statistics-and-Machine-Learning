930bded5febb9bca1a2b4679996feefdd495ae4e
you-chan2 <verticalbird2007@yahoo.co.jp>
2017-08-29 08:56:30 +0900

    【統計モデル】マルコフ切換えモデル

--- Marcov_switching_model.R
+++ Marcov_switching_model.R
@@ -8,20 +8,19 @@ library(ggplot2)
 library(lattice)
 
 ####データの発生####
-n <- 300   #サンプル数
-k1 <- 4   #切り替え数
-k2 <- 6   #観測確率のパラメータ数
+n <- 1000   #サンプル数
+k1 <- 3   #切り替え数
+k2 <- 2   #観測確率のパラメータ数
 
 
 ##初期確率の定義
-Pf <- c(0.4, 0.1, 0.3, 0.2)
+Pf <- c(0.4, 0.3, 0.3)
 
 ##推移行列の定義
-pr1 <- c(0.2, 0.1, 0.4, 0.3)
-pr2 <- c(0.1, 0.5, 0.2, 0.2)
-pr3 <- c(0.3, 0.1, 0.3, 0.3)
-pr4 <- c(0.1, 0.2, 0.3, 0.4)
-Pr <- rbind(pr1, pr2, pr3, pr4)
+pr1 <- c(0.1, 0.7, 0.2)
+pr2 <- c(0.2, 0.1, 0.7)
+pr3 <- c(0.7, 0.2, 0.1)
+Pr <- rbind(pr1, pr2, pr3)
 
 ##観測確率の定義
 P <- matrix(0, nrow=k1, ncol=k2)
@@ -30,6 +29,7 @@ for(i in 1:k1){
   P[i, ] <- rdirichlet(1, rep(alpha, k2))
 }
 
+P <-rbind(c(0.9, 0.1), c(0.6, 0.4), c(0.1, 0.9))
 
 ##応答変数の発生
 Z <- matrix(0, nrow=n, ncol=k1)
@@ -66,81 +66,87 @@ for(i in 1:k1){
 }
 y <- Y %*% 1:k2
 
-####EMアルゴリズム####
-##前向きアルゴリズムでalphaを推定
+#データの準備
+y_delta <- array(0, dim=c(n, k1, k2))
+for(i in 1:k2){
+  y_delta[, , i] <- matrix(Y[, i], nrow=n, ncol=k1)
+}
+
+#パラメータの格納用配列
 alpha <- matrix(0, nrow=n, ncol=k1)
-alpha1 <- matrix(0, nrow=n, ncol=k1)
 alpha_s <- matrix(0, nrow=n, ncol=k1)
 alpha_mu <- rep(0, n)
 B_vec <- matrix(0, nrow=n, ncol=k1)
-
-B_vec[1, ] <- B[, y[1]]
-alpha[1, ] <- rho * B_vec[1, ]
-alpha1[1, ] <- alpha[1, ]
-alpha_mu[1] <- 1 / sum(alpha[1, ])
-alpha_s[1, ] <- alpha_mu[1] * alpha[1, ] 
-
-for(i in 2:nrow(Y)){
- B_vec[i, ] <- B[, y[i]]
- alpha[i, ] <- (matrix(alpha_s[i-1, ], nrow=1, ncol=k1) %*% A) * B_vec[i, ]
- alpha1[i, ] <- alpha1[i-1, ] %*% A * B_vec[i, ]
- alpha_mu[i] <- 1 / sum(alpha[i, ])
- alpha_s[i, ] <- alpha[i, ] * alpha_mu[i]
-}
-
-##後ろ向きアルゴリズムでbetaを推定
 beta <- matrix(0, nrow=n, ncol=k1)
-beta1 <- matrix(0, nrow=n, ncol=k1)
 beta_s <- matrix(0, nrow=n, ncol=k1)
 
-beta[n, ] <- 1
-beta1[n, ] <- 1
-beta_s[n, ] <- alpha_mu[n]
+LL1 <- sum(log(apply(Y, 1, function(x) dmultinom(x, 1, B[1, ]))))   #対数尤度の初期値
+dl <- 100   #EMステップでの対数尤度の差の初期値
+tol <- 0.01
 
-for(i in n:2){
-  beta[i-1, ] <- A %*% (B_vec[i, ] * beta_s[i, ])
-  beta1[i-1, ] <- A %*% (B_vec[i, ] * beta1[i, ])
-  beta_s[i-1, ] <- beta[i-1, ] * alpha_mu[i-1, ] 
+L <- c()
+for(i in 1:n){
+  L <- c(L, dmultinom(Y[i, ], 1, P[which.max(Z[i, ]), ]))
 }
-
-##パラメータを更新
-A_vec <- matrix(A[1, ], nrow=n-1, ncol=k1, byrow=T)
-
-a11 <- alpha1[1:(n-1), ] * A_vec * B_vec[2:n, ] * beta1[2:n, ]
-a12 <- alpha1[1:(n-1), ] * beta1[1:(n-1), ] 
-
-a21 <- alpha_s[1:(n-1), ] * A_vec * B_vec[2:n, ] * beta_s[2:n, ]
-a22 <- alpha_s[1:(n-1), ] * beta_s[1:(n-1), ] / alpha_mu[1:(n-1), ]
-
-sum(colSums(a11)/colSums(a12))
-sum(colSums(a21)/colSums(a22))
+LT <- sum(log(L))
 
 
-P2 <- function(x, A, B, rho) {
-  # forward algorithm
-  N <- length(x)  # size of data
-  alpha <- rho * B[, x[1]]  # alpha1(i) = rho_i * b(i, x_1)
-  for (n in 2:N)
-    alpha <- (matrix(alpha, nrow=1, ncol=length(alpha)) %*% A) * B[, x[n]]
-  return(alpha)
-}
-
-P3 <- function(x, A, B, rho) {
-  # backward algorithm
-  N <- length(x)  # size of data
-  beta <- 1       # beta_n
-  for (n in N:2) {
-    # compute upto beta_1
-    beta <- A %*% matrix(B[, x[n]] * beta, ncol=1, nrow=nrow(B))
+####EMアルゴリズム(バウムウェルチアルゴリズム)で隠れマルコフモデルを推定####
+while(abs(dl) >= tol){
+  A <- Pr
+  B <- P
+  rho <- Pf
+  
+  ##前向きアルゴリズムでalphaを推定
+  B_vec[1, ] <- B[, y[1]]
+  alpha[1, ] <- rho * B_vec[1, ]
+  alpha_mu[1] <- 1 / sum(alpha[1, ])
+  alpha_s[1, ] <- alpha_mu[1] * alpha[1, ] 
+  
+  for(i in 2:n){
+    B_vec[i, ] <- B[, y[i]]
+    alpha[i, ] <- alpha_s[i-1, ] %*% A * B_vec[i, ]
+    alpha_mu[i] <- 1 / sum(alpha[i, ])
+    alpha_s[i, ] <- alpha[i, ] * alpha_mu[i]
   }
-  AA <- rho * beta * B[, x[1]] 
-  return(AA)
+  
+  ##後ろ向きアルゴリズムでbetaを推定
+  beta[n, ] <- 1
+  beta_s[n, ] <- alpha_mu[n]
+  
+  for(i in n:2){
+    beta[i-1, ] <- A %*% (B_vec[i, ] * beta_s[i, ])
+    beta_s[i-1, ] <- beta[i-1, ] * alpha_mu[i-1] 
+  }
+  
+  ##パラメータを更新
+  #推移確率Aを更新
+  for(i in 1:k1){
+    A_vec <- matrix(A[i, ], nrow=n-1, ncol=k1, byrow=T)
+    a11 <- matrix(alpha_s[1:(n-1), i], n-1, k1) * A_vec * B_vec[2:n, ] * beta_s[2:n, ]
+    a12 <- matrix(alpha_s[1:(n-1), i], n-1, k1) * matrix(beta_s[1:(n-1), i], n-1, k1) / alpha_mu[1:(n-1)]
+    a <- colSums(a11)/colSums(a12)
+    A[i, ] <- a
+  }
+  
+  
+  #観測データのパラメータBを更新
+  for(j in 1:k2){
+    B[, j] <- colSums(y_delta[, , j] * alpha_s * beta_s / alpha_mu) / colSums(alpha_s * beta_s / alpha_mu)
+  }
+  
+  #初期確率rhoを更新
+  rho <- alpha_s[1, ] * beta_s[1, ] / alpha_mu[1]
+  
+  ##対数尤度を計算
+  LL <- sum(log(1/alpha_mu))
+  dl <- LL - LL1
+  LL1 <- LL
+  print(LL)
 }
-beta_s[1, ]
-rho * beta1[2, ] *B[, Y[1, ]==1]/sum(rho * beta1[2, ] *B[, Y[1, ]==1])
-rho * beta1[2, ] * B_vec[1, ] 
-P3(Y %*% 1:k2, A, B, rho)
-beta1[1, ]
 
-P2(Y %*% 1:k2, A, B, rho)
+round(cbind(B, P), 3)
+round(cbind(A, Pr), 3)
+LT
+
 
