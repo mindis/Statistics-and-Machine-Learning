c7e52879602f94d6982c7a43c5513cf7d47f3d28
you-chan2 <verticalbird2007@yahoo.co.jp>
2017-09-08 08:54:35 +0900

    【統計モデル】入れ子型マルチレベルモデル

--- multilevel_linear_model.R
+++ multilevel_linear_model.R
@@ -37,9 +37,9 @@ X <- cbind(1, X.cont, X.bin, X.multi)
 
 ##アニメの割当を発生
 #パラメータの設定
-alpha0 <- runif(g1, -1.2, 1.0)
-alpha1 <- matrix(runif(g1*cont, 0, 0.6), nrow=cont, ncol=g1)
-alpha2 <- matrix(runif(g1*(bin+multi-1), -0.7, 0.6), nrow=bin+multi-1, ncol=g1)
+alpha0 <- runif(g1, -1.5, 0.9)
+alpha1 <- matrix(runif(g1*cont, 0, 0.7), nrow=cont, ncol=g1)
+alpha2 <- matrix(runif(g1*(bin+multi-1), -0.8, 0.6), nrow=bin+multi-1, ncol=g1)
 alpha <- rbind(alpha0, alpha1, alpha2)
 
 #ロジットと確率の計算
@@ -86,8 +86,81 @@ Z <- cbind(Z1, Z2, Z3)   #データを結合
 #評価していないアニメは欠損させる
 index_zeros <- subset(1:nrow(Z2), rowSums(Z2)==0)
 Z <- Z[-index_zeros, ]
-Z
+
+##IDの設定
+#ユーザーIDを設定
+freq <- colSums(Z[, 1:n])
+u.id <- rep(1:n, freq)
+
+#評価回数を設定
+t.id <- c()
+for(i in 1:n) {t.id <- c(t.id, 1:freq[i])}
+
+#アニメIDを設定
+a.id <- rep(0, nrow(Z))
+anime <- Z[, (n+1):(n+1+g1-1)]
+
+for(i in 1:ncol(anime)){
+  index <- subset(1:nrow(anime), anime[, i] > 0)
+  a.id[index] <- i
+}
+
+#キャラIDを設定
+c.id <- rep(0, nrow(Z))
+chara <- Z[, (n+1+g1):(ncol(Z))]
+
+for(i in 1:ncol(chara)){
+  index <- subset(1:nrow(chara), chara[, i] > 0)
+  c.id[index] <- i
+}
+
+#IDを結合
+ID <- data.frame(no=1:nrow(Z), t=t.id, u.id=u.id, a.id=a.id, c.id=c.id)
+table(ID$a.id); table(ID$c.id)
 
 
+##固定効果の説明変数をパネル形式に変更
+XM <- list()
+for(i in 1:n) {XM[[i]] <- matrix(X[i, ], nrow=freq[i], ncol=ncol(X), byrow=T)}
+X.panel <- do.call(rbind, XM)
+
+
+####応答変数(評価データ)の発生####
+##パラメータの設定
+#固定効果のパラメータ
+b.fix <- c(runif(1, 0.4, 0.7), runif(cont, 0, 0.6), runif(bin, -0.5, 0.7), runif(multi-1, -0.6, 0.9))   
+
+#変量効果のパラメータ
+random1 <- 1.0; random2 <- 1.5; random3 <- 1.25
+b.g1 <- rnorm(n, 0, random1)
+b.g2 <- rnorm(g1, 0, random2)
+b.g3 <- rnorm(g2s, 0, random3)
+b.random <- c(b.g1, b.g2, b.g3)
+
+#個体内分散の設定
+Cov <- 0.8
+
+##応答変数を発生
+mu <- X.panel %*% b.fix  + Z %*% b.random   #平均構造
+y <- mu + rnorm(length(mu), 0, Cov)   #平均構造 + 誤差
+
+
+####マルコフ連鎖モンテカルロ法でマルチレベルモデルの推定####
+#アルゴリズムの設定
+R <- 20000
+sbeta <- 1.5
+keep <- 4
+
+##事前分布の設定
+#固定効果の事前分布
+beta_prior <- rep(0, ncol(X))   #固定効果の回帰係数の事前分布の平均
+sigma_prior <- 0.01*diag(ncol(X))   #固定効果の回帰係数の事前分布の分散
+tau_prior1 <- 0.01   #逆ガンマ分布の形状パラメータ
+tau_prior2 <- 0.01   #逆ガンマ分布のスケールパラメータ
+
+#変量効果の事前分布
+alpha_random <- 0   #変量効果の事前分布の平均
+tau_random1 <- 1   #逆ガンマ分布の形状パラメータ
+tau_random2 <- 0.01   #逆ガンマ分布のスケールパラメータ
 
 
