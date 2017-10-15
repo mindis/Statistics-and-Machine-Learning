2205d5e68c688be681ab6d9022fd0f00c14f91a2
you-chan2 <verticalbird2007@yahoo.co.jp>
2017-10-16 08:42:18 +0900

    【統計モデル】有限混合L1正則化多項-二値離散選択モデル

--- mixture_multinomial_binomial_logit_model.R
+++ mixture_multinomial_binomial_logit_model.R
@@ -21,10 +21,11 @@ seg <- 2
 
 ##セグメント割当の設定
 seg_id <- rep(1:seg, rep(hh/seg, seg))
+z_seg <- cbind(rep(1:0, rep(hh/seg, seg)), rep(0:1, rep(hh/seg, seg)))
 
 ##説明変数の発生
-k <- 150   #説明変数数
-freq <- rpois(hh, 100)   #ポアソン分布から頻度を発生
+k <- 300   #説明変数数
+freq <- rpois(hh, 200)   #ポアソン分布から頻度を発生
 p <- rdirichlet(hh, runif(k, 0.2, 1.0))   #ディレクリ分布から出現確率を発生
 X <- t(apply(cbind(freq, p), 1, function(x) rmultinom(1, x[1], x[-1])))   #多項分布から説明変数を発生
 XM <- cbind(1, X)
@@ -35,8 +36,8 @@ for(i in 1:1000){
   print(i)
     
   ##多項ロジットモデルのパラメータの設定
-  b00 <- matrix(rnorm((k+1)*(select-1)*seg, 0, 0.35), nrow=k+1, ncol=(select-1)*seg)
-  b01 <- ifelse(abs(b00) > 0.1, b0, 0)
+  b00 <- matrix(rnorm((k+1)*(select-1)*seg, 0, 0.40), nrow=k+1, ncol=(select-1)*seg)
+  b01 <- ifelse(abs(b00) > 0.1, b00, 0)
   b01[1, ] <- runif((select-1)*seg, -1.2, 1.4)
   
   #有効な回帰係数は説明変数の30％
@@ -47,8 +48,8 @@ for(i in 1:1000){
   beta0 <- list(beta01, beta02)
   
   ##複数選択があるかどうかのロジスティック回帰モデルのパラメータの設定
-  a00 <- matrix(rnorm((k+1)*seg, 0, 0.25), nrow=k+1, seg)
-  a01 <- ifelse(abs(a00) > 0.1, b0, 0)
+  a00 <- matrix(rnorm((k+1)*seg, 0, 0.30), nrow=k+1, seg)
+  a01 <- ifelse(abs(a00) > 0.1, a00, 0)
   a01[1, ] <- c(-1.1, 0.8)
   
   #有効な回帰係数は説明変数の30％
@@ -70,7 +71,7 @@ for(i in 1:1000){
   Pr12 <- exp(mlogit2) / matrix(rowSums(exp(mlogit2)), nrow=nrow(mlogit2), ncol=select)
   Pr21 <- exp(blogit1) / (1+exp(blogit1))
   Pr22 <- exp(blogit2) / (1+exp(blogit2))
-  if((mean(Pr22)-mean(Pr21)) > 0.3 & mean(Pr22) > 0.55 & mean(Pr21) > 0.2) break
+  if((mean(Pr22)-mean(Pr21)) > 0.3 & mean(Pr22) > 0.60 & mean(Pr22) < 0.85 & mean(Pr21) > 0.2 & mean(Pr21) < 0.4) break
 }
 
 #確率を結合
@@ -89,22 +90,22 @@ y2 <- rbinom(hh, 1, Pr2)
 obsll <- function(alpha, beta, y1, y2, X, r, seg, hh){
   
   #パラメータの格納用配列
-  Pr1 <- list()
+  Pr1 <- array(0, dim=c(nrow(y1), ncol(y1), seg))
   Pr2 <- matrix(0, nrow=hh, ncol=seg)
   Li1 <- matrix(0, nrow=hh, ncol=seg)
   Li2 <- matrix(0, nrow=hh, ncol=seg)
 
   for(j in 1:seg){
   #ロジットの計算
-    mlogit <- X %*% cbind(0, beta[[j]])
+    mlogit <- X %*% beta[, , j]
     blogit <- X %*% alpha[, j]
     
     #確率の計算
-    Pr1[[j]] <- exp(mlogit) / matrix(rowSums(exp(mlogit)), nrow=nrow(mlogit), ncol=select)
+    Pr1[, , j] <- exp(mlogit) / matrix(rowSums(exp(mlogit)), nrow=nrow(mlogit), ncol=select)
     Pr2[, j] <- exp(blogit) / (1+exp(blogit))
     
     #サンプルごとに尤度を格納
-    Li1[, j] <- exp(rowSums(y1 * log(Pr1[[j]])))
+    Li1[, j] <- exp(rowSums(y1 * log(Pr1[, , j])))
     Li2[, j] <- exp(y2*log(Pr2[, j]) + (1-y2)*log(1-Pr2[, j]))
   }
   
@@ -125,7 +126,7 @@ obsll <- function(alpha, beta, y1, y2, X, r, seg, hh){
 ##EMアルゴリズムの設定
 iter <- 0
 dl <- 100   #EMステップでの対数尤度の初期値の設定
-tol <- 1
+tol <- 0.1
 
 ##EMアルゴリズムのlambdaと初期値の設定
 #クロスバリデーションでlambdaを決定
@@ -193,10 +194,66 @@ beta <- ifelse(abs(beta02) < 0.1, 0, beta02)
 
 #二項ロジットモデルのパラメータの格納
 alpha01 <- matrix(0, nrow=k+1, ncol=seg)
-alpha01[1, ] <- out2$a0
+alpha01[1, ] <- c(-1.0, 1.0)
 alpha01[-1, ] <- matrix(out2$beta, nrow=k, ncol=seg)
 
 alpha02<- alpha01 + rnorm(length(alpha01), 0, 0.15)
 alpha <- ifelse(abs(alpha01) < 0.1, 0, alpha02)
+
+r <- rep(0.5, seg)   #混合率の初期値
+
+##対数尤度と潜在変数zの初期化
+L <- obsll(alpha, beta, Y1, y2, XM, r, seg,hh)
+z1 <- L$z1
+LL1 <- L$LLobz
+
+
+##EMアルゴリズムでパラメータを推定
+while(abs(dl) >= tol){
+
+  ##L1正則化ロジットモデルを推定(Mステップ)
+  out1 <- list()
+  out2 <- list()
+  
+  #L1正則化多項ロジットおよび二項ロジットモデルを推定
+  for(j in 1:seg){
+    out1[[j]] <- glmnet(X, y1, family="multinomial", lambda=lambda1, standardize=FALSE, alpha=1, weights=z1[, j])
+    out2[[j]] <- glmnet(X, y2, family="binomial", lambda=lambda2, standardize=FALSE, alpha=1, weights=z1[, j])
+    
+    #パラメータの更新
+    for(i in 1:select){
+      beta[1, i, j] <- out1[[j]]$a0[i]
+      beta[-1, i, j] <- as.numeric(out1[[j]]$beta[[i]])
+    }
+    alpha[, j] <- c(out2[[j]]$a0 ,as.numeric(out2[[j]]$beta))
+  }
+  
+  r <- colSums(z1)/hh   #混合率の更新
+  
+  ##観測データの対数尤度を評価(Eステップ)
+  #観測データの対数尤度と潜在変数zの更新
+  obzll <- obsll(alpha, beta, Y1, y2, XM, r, seg,hh)
+  z1 <- obzll$z1
+  LL <- obzll$LLobz
+  
+  #アルゴリズムの収束判定
+  iter <- iter + 1
+  dl <- LL - LL1
+  LL1 <- LL
+  print(LL)
+}
+
+####推定結果の確認と適合度####
+##推定されたパラメータと真のパラメータの比較
+round(cbind(alpha, alpha0), 3)   #二項ロジットの回帰パラメータ
+round(cbind(beta[, , 1], beta[, , 2], beta0[[1]], beta0[[2]]), 2)   #多項ロジットの回帰パラメータ
+round(rbind(r, r0=table(seg_id)/hh), 3)   #混合率
+round(cbind(z1, seg=seg_id), 3)
+
+##適合度
+round(LL <- obzll$LLobz, 3)   #観測データの対数尤度
+round(-2*(LL) + 2*(length(theta)+length(r)), 3) #AIC
+
+beta
 
 
