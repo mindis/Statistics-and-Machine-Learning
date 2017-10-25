f1c3798c90e14472861c19a7bb600d9b0c277f9d
you-chan2 <verticalbird2007@yahoo.co.jp>
2017-10-26 08:53:54 +0900

    【統計モデル】入れ子型トピックモデル

--- nested_PLSA.R
+++ nested_PLSA.R
@@ -2,6 +2,7 @@
 library(MASS)
 library(lda)
 library(RMeCab)
+library(matrixStats)
 detach("package:bayesm", unload=TRUE)
 library(extraDistr)
 library(reshape2)
@@ -48,39 +49,57 @@ for(i in 1:hh){
   w <- c(w, rpois(freq[i], par))   #購買数
 }
 
-
 ####bag of word形式の文書行列を発生####
 #パラメータの設定
-alpha0 <- round(runif(k, 0.1, 1.25), 3)   #ユーザーのディレクリ事前分布のパラメータ
-alpha1 <- round(runif(k, 0.1, 1.0), 3)   #アイテムのディレクリ事前分布のパラメータ
-alpha2 <- rep(0.25, v)   #単語のディレクリ事前分布のパラメータ
-
+alpha0 <- round(runif(k, 0.2, 0.3), 3)   #ユーザーのディレクリ事前分布のパラメータ
+alpha1 <- round(runif(k, 0.15, 0.3), 3)   #アイテムのディレクリ事前分布のパラメータ
+alpha2 <- rep(0.125, v)   #単語のディレクリ事前分布のパラメータ
+alpha0
 #ディレクリ乱数の発生
 beta <- rdirichlet(hh, alpha0)   #ユーザーのトピック分布をディレクリ乱数から発生
 gamma <- rdirichlet(item, alpha1)   #アイテムのトピック分布をディレクリ乱数から発生 
 phi <- rdirichlet(k, alpha2)   #単語のトピック分布をディレクリ乱数から発生
+pi <- rbeta(sum(w), 0.3, 0.35)
 
+#スイッチングラベル選択のためのインデックスを作成
+index_list <- list()
+index_pi <- list()
+for(i in 1:d){index_list[[i]] <- rep(i, w[i])}
+index <- unlist(index_list)
+for(i in 1:d){index_pi[[i]] <- subset(1:length(index), index==i)}
 
 #多項分布の乱数からデータを発生
 WX <- matrix(0, nrow=d, ncol=v)
-theta <- matrix(0, nrow=d, ncol=k)
+Pi <- matrix(0, nrow=d, ncol=v)
+theta1 <- matrix(0, nrow=d, ncol=k)
+theta2 <- matrix(0, nrow=d, ncol=k)
 Z <- list()
+y <- list()
 
 for(i in 1:hh){
+  
   r1 <- i.id[u.id==i]
+  u1 <- u.id[u.id==i]
   index1 <- subset(1:length(u.id), u.id==i)
   
   for(j in 1:sum(u.id==i)){
+    
     #インデックスを設定
     r2 <- r1[j]   #アイテムインデックス
+    u2 <- u1[j]   #ユーザーインデックス
     index2 <- index1[j]   #文書インデックス
     
-    #アイテムとユーザートピックの同時確率
-    par <- beta[i, ] * gamma[r2, ]
-    theta[index2, ] <- par / sum(par)
-  
     #多項分布より文書トピックを生成
-    z <- t(rmultinom(w[index2], 1, theta[index2, ]))   #文書のトピック分布を発生
+    #文書のトピック分布を発生
+    z1 <- t(rmultinom(w[index2], 1, beta[u2, ]))   #ユーザーのトピックを発生   
+    z2 <- t(rmultinom(w[index2], 1, gamma[r2, ]))   #アイテムのトピックの発生
+    
+    #どちらのトピックから発生したかを決定
+    y[[index2]] <- rbinom(w[index2], 1, pi[index_pi[[index2]]])
+    Y <- matrix(y[[index2]], nrow=w[index2], ncol=k)
+    z <- Y*z1 + (1-Y)*z2
+
+    #トピックをベクトル形式に変換
     zn <- z %*% c(1:k)   #0,1を数値に置き換える
     zdn <- cbind(zn, z)   #apply関数で使えるように行列にしておく
     
@@ -89,15 +108,24 @@ for(i in 1:hh){
     wdn <- colSums(wn)   #単語ごとに合計して1行にまとめる
     WX[index2, ] <- wdn  
     Z[[index2]] <- zdn[, 1]
+    
+    #トピックの発生確率を格納しておく
+    freq_w0 <- colSums(wn)
+    freq_w <- ifelse(freq_w0==0, 1, freq_w0)
+    Pi[index2, ] <- colSums(matrix(pi[index_pi[[index2]]], nrow=length(pi[index_pi[[index2]]]), ncol=v) * wn) / freq_w
   }
   print(i)
 }
+
+storage.mode(WX) <- "integer"
+gc(); gc()
 
 ####EMアルゴリズムでトピックモデルを推定####
 ####トピックモデルのためのデータと関数の準備####
 ##それぞれの文書中の単語の出現をベクトルに並べる
 ##データ推定用IDを作成
 ID_list <- list()
+pi_list <- list()
 user_list <- list()
 item_list <- list()
 wd_list <- list()
@@ -105,14 +133,25 @@ wd_list <- list()
 #文書ごとにユーザーIDおよび単語IDを作成
 for(i in 1:nrow(WX)){
   print(i)
+  
+  #IDを格納
   ID_list[[i]] <- rep(i, w[i])
   user_list[[i]] <- rep(ID$id[i], w[i])
   item_list[[i]] <- rep(ID$item[i], w[i])
-  num1 <- (WX[i, ] > 0) * c(1:v) 
-  num2 <- subset(num1, num1 > 0)
+  
+  #出現単語をベクトル化して格納
+  num11 <- (WX[i, ] > 0) * c(1:v) 
+  num12 <- subset(num11, num11 > 0)
   W1 <- WX[i, (WX[i, ] > 0)]
-  number <- rep(num2, W1)
-  wd_list[[i]] <- number
+  number1 <- rep(num12, W1)
+  wd_list[[i]] <- number1
+  
+  #トピック発生確率をベクトル化して格納
+  num21 <- Pi[i, ] * rep(1, v) 
+  num22 <- subset(num21, num21 > 0)
+  P1 <- WX[i, (WX[i, ] > 0)]
+  number2 <- rep(num22, P1)
+  pi_list[[i]] <- number2
 }
 
 #リストをベクトルに変換
@@ -120,6 +159,7 @@ ID_d <- unlist(ID_list)
 ID_u <- unlist(user_list)
 ID_i <- unlist(item_list)
 wd <- unlist(wd_list)
+pd <- unlist(pi_list)
 
 ##インデックスを作成
 doc_list <- list()
@@ -146,6 +186,24 @@ burden_fr <- function(theta, phi, wd, w, k){
   bval <- list(Br=Br, Bur=Bur, r=r)
   return(bval)
 }
+
+
+Bur1 <- matrix(0, nrow=length(ID_i), ncol=k)
+Bur2 <- matrix(0, nrow=length(ID_i), ncol=k)
+
+for(kk in 1:k){
+  #負担係数を計算
+  Bi1 <- beta[ID_u, kk] * phi[kk, c(wd)]   #尤度
+  Bi2 <- gamma[ID_i, kk] * phi[kk, c(wd)]   #尤度
+  Bur1[, kk] <- Bi1   
+  Bur2[, kk] <- Bi2
+}
+
+a1 <- rowSums(Bur1)
+a2 <- rowSums(Bur2)
+
+round(cbind(a1/(a1+a2), pd), 3)
+
 
 ####EMアルゴリズムの初期値を設定する####
 ##初期値をランダムに設定
