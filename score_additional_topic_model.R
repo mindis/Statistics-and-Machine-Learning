6c248716e2ff802d97b0293ddcb3d3a85815a2d8
you-chan2 <verticalbird2007@yahoo.co.jp>
2017-12-21 08:39:30 +0900

    【統計モデル】評点つきトピックモデル

--- score_additional_topic_model.R
+++ score_additional_topic_model.R
@@ -1,4 +1,4 @@
-#####評点つきトピックモデル####
+#####教師ありトピックモデル####
 library(MASS)
 library(lda)
 library(RMeCab)
@@ -15,14 +15,13 @@ library(ggplot2)
 ####データの発生####
 #set.seed(423943)
 #データの設定
-k <- 10   #トピック数
-d <- 2000   #文書数
-v1 <- 250   #評点に関係のある語彙数
-v2 <- 100   #評点に関係のない語彙数
+k <- 6   #教師数
+d <- 3000   #文書数
+v1 <- 300   #教師に関係のある語彙数
+v2 <- 100   #教師に関係のない語彙数
 v <- v1 + v2   #語彙数
 w <- rpois(d, rgamma(d, 55, 0.50))   #1文書あたりの単語数
 f <- sum(w)
-s <- 5   #評点クラス数
 
 #インデックスを作成
 id_vec <- rep(1:d, w)
@@ -31,38 +30,48 @@ for(i in 1:d){
   index_hh[[i]] <- which(id_vec==i)
 }
 
+#教師データを生成
+pr <- runif(k, 1, 5)
+Y <- rmnom(d, 1, pr)
+y <- as.numeric(Y %*% 1:k)
+y_vec <- as.numeric(y)[id_vec] 
+
 
 #パラメータの設定
-alpha0 <- rep(0.15, k)   #文書のディレクリ事前分布のパラメータ
+alpha0 <- alpha0 <- diag(1.5, k) + 0.15   #文書のディレクリ事前分布のパラメータ
 alpha1 <- c(rep(0.4, v1), rep(0.005, v2))   #評点に関係のある単語のディレクリ事前分布のパラメータ
-alpha2 <- c(rep(0.05, v1), rep(5, v2))   #評点に関係のない単語のディレクリ事前分布のパラメータ
-alpha3 <- rep(0.4, s)   #評点のディレクリ事前分布のパラメータ
+alpha2 <- c(rep(0.05, v1), rep(5, v2))   #教師に関係のない単語のディレクリ事前分布のパラメータ
 
 #ディレクリ乱数の発生
-thetat <- theta <- rdirichlet(d, alpha0)   #文書のトピック分布をディレクリ乱数から発生
+thetat <- theta <- rdirichlet(d, alpha0[y, ])   #文書のトピック分布をディレクリ乱数から発生
 phit <- phi <- rdirichlet(k, alpha1)   #評点に関係のある単語分布をディレクリ乱数から発生
 gammat <- gamma <- rdirichlet(1, alpha2)   #評点に関係のない単語分布をディレクリ乱数から発生
-betat <- beta <- rbeta(sum(f), 0.55, 0.175)   #単語が評点と関連するかどうかのパラメータ
+betat <- beta <- rbeta(sum(f), 15, 15)   #単語が評点と関連するかどうかのパラメータ
 
-#多項分布の乱数からデータを発生
+
+##多項分布の乱数からデータを発生
 WX <- matrix(0, nrow=d, ncol=v)
+x_list <- list()
 x <- rep(0, f)
-y <- matrix(0, nrow=d, ncol=s)
 Z <- list()
 index_v1 <- 1:v1
 index_v2 <- (v1+1):v
 
-
 for(i in 1:d){
+  #文書のトピックを生成
   z <- rmnom(w[i], 1, theta[i, ])   #文書のトピック分布を発生
   z_vec <- z %*% c(1:k)   #トピック割当をベクトル化
   
-  x[index_hh[[i]]] <- rbinom(w[i], 1, beta[index_hh[[i]]])
+  #一般語かどうかを生成
+  x_list[[i]] <- rbinom(w[i], 1, beta[index_hh[[i]]])
   
-  wn <- rmnom(w[i], 1, phi[z_vec, ])   #文書のトピックから単語を生成
-  wdn <- colSums(wn)   #単語ごとに合計して1行にまとめる
+  #生成したトピックから単語を生成
+  wn <- rmnom(sum(x_list[[i]]), 1, phi[z_vec[x_list[[i]]==1], ])   #文書のトピックから単語を生成
+  an <- rmnom(sum(1-x_list[[i]]), 1, gammat)
+  wdn <- colSums(wn) + colSums(an)   #単語ごとに合計して1行にまとめる
   WX[i, ] <- wdn
   Z[[i]] <- z
+  x[index_hh[[i]]] <- x_list[[i]]
   print(i)
 }
 
@@ -112,7 +121,7 @@ burden_fr <- function(theta, phi, wd, w, k){
   bval <- list(Br=Br, Bur=Bur, r=r)
   return(bval)
 }
-
+  
 ##アルゴリズムの設定
 R <- 10000   #サンプリング回数
 keep <- 2   #2回に1回の割合でサンプリング結果を格納
@@ -121,27 +130,67 @@ burnin <- 1000/keep
 
 ##事前分布の設定
 #ハイパーパラメータの事前分布
-alpha01 <- rep(1.0, k)
-beta0 <- rep(0.5, v)
-alpha01m <- matrix(alpha01, nrow=d, ncol=k, byrow=T)
-beta0m <- matrix(beta0, nrow=k, ncol=v)
+alpha01 <- 1.0
+alpha02 <- 0.5
 
 
 ##パラメータの初期値
-theta <- rdirichlet(d, rep(1, k))   #文書トピックのパラメータの初期値
-phi <- rdirichlet(k, colSums(WX)/sum(WX)*v)   #単語トピックのパラメータの初期値
+#トピック分布の初期値
+theta <- extraDistr::rdirichlet(d, (diag(1.0, k) + 0.25)[y, ])   #文書トピックのパラメータの初期値
+
+#一般語の単語分布の初期値
+inv_idf <- colSums(WX > 0)/d
+gamma <- inv_idf / sum(inv_idf)
+
+#教師関連単語分布の初期値
+phi <- matrix(0, nrow=k, ncol=v)
+for(j in 1:k) {
+  M <- colSums(WX[y==j, ])
+  phi[j, ] <- M*log(1/inv_idf) / sum(M*log(1/inv_idf))
+}
+
 
 ##パラメータの格納用配列
 THETA <- array(0, dim=c(d, k, R/keep))
 PHI <- array(0, dim=c(k, v, R/keep))
-SEG <- matrix(0, nrow=f, ncol=k)
-storage.mode(SEG) <- "integer"
+GAMMA <- matrix(0, nrow=R/keep, ncol=v)
+SEG1 <- matrix(0, nrow=f, ncol=k)
+SEG2 <- rep(0, nrow=f)
+storage.mode(SEG1) <- "integer"
 gc(); gc()
 
 ##MCMC推定用配列
 wsum0 <- matrix(0, nrow=d, ncol=k)
 vf0 <- matrix(0, nrow=k, ncol=v)
 
+
+##単語ごとに尤度と負担率を計算する関数
+burden_fr <- function(theta, phi, wd, w, k){
+  Bur <-  matrix(0, nrow=length(wd), ncol=k)   #負担係数の格納用
+  for(j in 1:k){
+    #負担係数を計算
+    Bi <- rep(theta[, j], w) * phi[j, wd]   #尤度
+    Bur[, j] <- Bi   
+  }
+  
+  Br <- Bur / rowSums(Bur)   #負担率の計算
+  r <- colSums(Br) / sum(Br)   #混合率の計算
+  bval <- list(Br=Br, Bur=Bur, r=r)
+  return(bval)
+}
+
+rep(theta[, j], w) 
+phi[y_vec, ]
+
+
+y_vec
+M <- matrix(0, nrow=f, ncol=k+1)
+for(j in 1:k){
+  M[, j] <- phit[j, wd]
+}
+M[, k+1] <- gammat[wd] 
+
+round(cbind(y_vec, wd, M/rowSums(M)), 3)
 
 ####ギブスサンプリングでパラメータをサンプリング####
 for(rp in 1:R){
