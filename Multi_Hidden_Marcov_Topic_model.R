e922c861e72e12ef79ff14f018d10540bb6786c9
you-chan2 <verticalbird2007@yahoo.co.jp>
2018-01-16 08:54:30 +0900

    【統計モデル】Multi Hidden Marcov Topic model

--- Multi_Hidden_Marcov_Topic_model.R
+++ Multi_Hidden_Marcov_Topic_model.R
@@ -59,128 +59,136 @@ alpha02 <- matrix(0.3, nrow=k1, ncol=k1)
 diag(alpha02) <- 1.5
 alpha03 <- rep(0.2, k2)
 alpha04 <- rep(0.2, k3)
-alpha11 <- c(rep(0.2, v1), rep(0.001, v2))
-alpha12 <- c(rep(0.0001, v1), rep(0.2, v2))
+alpha11 <- c(rep(0.1, v1), rep(0.0005, v2))
+alpha12 <- c(rep(0.0005, v1), rep(0.1, v2))
 
-#パラメータを生成
-theta1 <- thetat1 <- extraDistr::rdirichlet(1, alpha01)
-theta2 <- thetat2 <- extraDistr::rdirichlet(k1, alpha02)
-theta3 <- thetat3 <- extraDistr::rdirichlet(k1, alpha03) 
-theta4 <- thetat4 <- extraDistr::rdirichlet(d, alpha04)
-gamma <- gammat <- extraDistr::rdirichlet(k2, alpha11)
-phi <- phit <- extraDistr::rdirichlet(k3, alpha12)
-omega <- omegat <- rbeta(d, 25.0, 27.5)
-
-##モデルにもとづき単語を生成する
-wd_list <- list()
-WX <- matrix(0, nrow=d, ncol=v)
-ID_list <- list()
-td_list <- list()
-Z1_list <- list()
-Z2_list <- list()
-Z3_list <- list()
-Z4_list <- list()
-
-#単語ごとに文章共通か文章固有かを生成
-z1_vec <- rbinom(f, 1, omega[ID_d])
-
-#文章ごとのセグメントを生成
-z2_vec2 <- z2_vec <- as.numeric(rmnom(a, 1, theta1) %*% 1:k1)
-freq <- c()
-
-for(i in 1:a){
-  flag <- sum(1-z1_vec[s_list[[i]]])
-  freq <- c(freq, flag)
-  if(t_id[i]==1){
-    if(flag==0){
-     z2_vec[i] <- 0 
-     next
-    } 
-    if(flag > 0){
-      next
+for(l in 1:100){
+  print(l)
+  #パラメータを生成
+  theta1 <- thetat1 <- extraDistr::rdirichlet(1, alpha01)
+  theta2 <- thetat2 <- extraDistr::rdirichlet(k1, alpha02)
+  theta3 <- thetat3 <- extraDistr::rdirichlet(k1, alpha03) 
+  theta4 <- thetat4 <- extraDistr::rdirichlet(d, alpha04)
+  gamma <- gammat <- extraDistr::rdirichlet(k2, alpha11)
+  phi <- phit <- extraDistr::rdirichlet(k3, alpha12)
+  omega <- omegat <- rbeta(d, 25.0, 27.5)
+  
+  ##モデルにもとづき単語を生成する
+  wd_list <- list()
+  WX <- matrix(0, nrow=d, ncol=v)
+  ID_list <- list()
+  td_list <- list()
+  Z1_list <- list()
+  Z2_list <- list()
+  Z3_list <- list()
+  Z4_list <- list()
+  
+  #単語ごとに文章共通か文章固有かを生成
+  z1_vec <- rbinom(f, 1, omega[ID_d])
+  
+  #文章ごとのセグメントを生成
+  z2_vec2 <- z2_vec <- as.numeric(rmnom(a, 1, theta1) %*% 1:k1)
+  freq <- c()
+  
+  for(i in 1:a){
+    flag <- sum(1-z1_vec[s_list[[i]]])
+    freq <- c(freq, flag)
+    if(t_id[i]==1){
+      if(flag==0){
+       z2_vec[i] <- 0 
+       next
+      } 
+      if(flag > 0){
+        next
+      }
+    }
+    if(t_id[i]!=0){
+      if(z2_vec[i-1]==0 & flag==0){
+        z2_vec[i] <- 0
+        next
+      }
+      if(z2_vec[i-1]==0 & flag > 0){
+        next
+      }
+      if(z2_vec[i-1]!=0 & flag > 0){
+        z2 <- rmnom(1, 1, theta2[z2_vec[i-1], ])
+        z2_vec[i] <- as.numeric(z2 %*% 1:k1)
+        next
+      }
+      if(z2_vec[i-1]!=0 & flag==0){
+        z2_vec[i] <- z2_vec[i-1]
+      }
     }
   }
-  if(t_id[i]!=0){
-    if(z2_vec[i-1]==0 & flag==0){
-      z2_vec[i] <- 0
-      next
+  Z1 <- z1_vec
+  Z2 <- z2_vec
+  
+  #文章ごとに逐次的にトピックと単語を生成
+  Z3_list <- list()
+  Z4_list <- list()
+  wd_list <- list()
+  WX <- matrix(0, nrow=a, ncol=v)
+  
+  for(i in 1:a){
+    if(i%%1000==0){
+      print(i)
     }
-    if(z2_vec[i-1]==0 & flag > 0){
-      next
+    #パラメータの格納用配列
+    index_id <- u_id[i]
+    n <- w[i]
+    z3_vec <- rep(0, n)
+    z4_vec <- rep(0, n)
+    wd <- rep(0, n)
+    
+    #文書共通か文書固有かの指示変数を取り出す
+    flag <- z1_vec[s_list[[i]]]
+    index1 <- which(flag==0)
+    index2 <- which(flag==1)
+    
+    #文章共通のトピックを生成
+    if(length(index1) > 0){
+      z3 <- rmnom(length(index1), 1, theta3[z2_vec[i], ]) 
+      z3_vec[index1] <- as.numeric(z3 %*% 1:k2)
     }
-    if(z2_vec[i-1]!=0 & flag > 0){
-      z2 <- rmnom(1, 1, theta2[z2_vec[i-1], ])
-      z2_vec[i] <- as.numeric(z2 %*% 1:k1)
-      next
+  
+    #文書固有のトピックの生成
+    if(length(index2) > 0){
+      z4 <- rmnom(length(index2), 1, theta4[index_id, ]) 
+      z4_vec[index2] <- as.numeric(z4 %*% 1:k3)
     }
-    if(z2_vec[i-1]!=0 & flag==0){
-      z2_vec[i] <- z2_vec[i-1]
+    
+    #文章共通のトピックから単語を生成
+    index_topic1 <- z3_vec[z3_vec!=0]
+    if(length(index_topic1) > 0){
+      wd1 <- rmnom(length(index_topic1), 1, gamma[index_topic1, ])
+      wd[index1] <- as.numeric(wd1 %*% 1:v)
     }
+    
+    #文書固有のトピックから単語を生成
+    index_topic2 <- z4_vec[z4_vec!=0]
+    if(length(index_topic2) > 0){
+      wd2 <- rmnom(length(index_topic2), 1, phi[index_topic2, ]) 
+      wd[index2] <- as.numeric(wd2 %*% 1:v)
+    }
+    
+    #パラメータを格納
+    wd_list[[i]] <- wd 
+    WX[i, ] <- colSums(wd1) + colSums(wd2)
+    Z3_list[[i]] <- z3_vec
+    Z4_list[[i]] <- z4_vec
+  }
+  
+  #リストをベクトル変換
+  Z3 <- unlist(Z3_list)
+  Z4 <- unlist(Z4_list[[i]])
+  wd <- unlist(wd_list)
+  if(length(unique(wd))==v){
+    break
   }
 }
-Z1 <- z1_vec
-Z2 <- z2_vec
-
-#文章ごとに逐次的にトピックと単語を生成
-Z3_list <- list()
-Z4_list <- list()
-wd_list <- list()
-WX <- matrix(0, nrow=a, ncol=v)
-
-for(i in 1:a){
-  if(i%%1000==0){
-    print(i)
-  }
-  #パラメータの格納用配列
-  index_id <- u_id[i]
-  n <- w[i]
-  z3_vec <- rep(0, n)
-  z4_vec <- rep(0, n)
-  wd <- rep(0, n)
-  
-  #文書共通か文書固有かの指示変数を取り出す
-  flag <- z1_vec[s_list[[i]]]
-  index1 <- which(flag==0)
-  index2 <- which(flag==1)
-  
-  #文章共通のトピックを生成
-  if(length(index1) > 0){
-    z3 <- rmnom(length(index1), 1, theta3[z2_vec[i], ]) 
-    z3_vec[index1] <- as.numeric(z3 %*% 1:k2)
-  }
-
-  #文書固有のトピックの生成
-  if(length(index2) > 0){
-    z4 <- rmnom(length(index2), 1, theta4[index_id, ]) 
-    z4_vec[index2] <- as.numeric(z4 %*% 1:k3)
-  }
-  
-  #文章共通のトピックから単語を生成
-  index_topic1 <- z3_vec[z3_vec!=0]
-  if(length(index_topic1) > 0){
-    wd1 <- rmnom(length(index_topic1), 1, gamma[index_topic1, ])
-    wd[index1] <- as.numeric(wd1 %*% 1:v)
-  }
-  
-  #文書固有のトピックから単語を生成
-  index_topic2 <- z4_vec[z4_vec!=0]
-  if(length(index_topic2) > 0){
-    wd2 <- rmnom(length(index_topic2), 1, phi[index_topic2, ]) 
-    wd[index2] <- as.numeric(wd2 %*% 1:v)
-  }
-  
-  #パラメータを格納
-  wd_list[[i]] <- wd 
-  WX[i, ] <- colSums(wd1) + colSums(wd2)
-  Z3_list[[i]] <- z3_vec
-  Z4_list[[i]] <- z4_vec
-}
-
-#リストをベクトル変換
-Z3 <- unlist(Z3_list)
-Z4 <- unlist(Z4_list[[i]])
-wd <- unlist(wd_list)
-
+Data <- matrix(as.numeric(table(1:f, wd)), nrow=f, ncol=v)
+sparse_data <- as(Data, "CsparseMatrix")
+rm(Data)
 
 ##インデックスを作成
 doc_list <- list()
@@ -204,6 +212,7 @@ burden_fr <- function(theta, phi, wd, w, k){
   return(bval)
 }
 
+
 ####MHMMトピックモデルのMCMCアルゴリズムの設定####
 ##アルゴリズムの設定
 R <- 10000
@@ -220,6 +229,7 @@ theta4 <- thetat4
 gamma <- gammat
 phi <- phit
 omega <- omegat
+z2_vec <- Z2
 
 ##LDAからHTMモデルの初期値を設定
 theta1
@@ -259,133 +269,141 @@ storage.mode(SEG4) <- "integer"
 
 
 ##MCMC推定用配列
+max_time <- max(t_id)
 max_word <- max(words)
-index_t11 <- which(td_d==1)
+index_t11 <- which(t_id==1)
 index_t21 <- list()
 index_t22 <- list()
 for(j in 2:max_word){
-  index_t21[[j]] <- which(td_d==j)-1
-  index_t22[[j]] <- which(td_d==j)
+  index_t21[[j]] <- which(t_id==j)-1
+  index_t22[[j]] <- which(t_id==j)
 }
 
 
 ####ギブスサンプリングでHTMモデルのパラメータをサンプリング####
 for(rp in 1:R){
   
-  ##文章ごとにHMMに基づきセグメントを生成
-  
-  
-  
   ##単語ごとに文書共通か文書固有かを生成
   #単語ごとに文書共通のトピック尤度を計算
+  z2_indicate <- z2_vec[nd_d]
+  index_zeros <- which(z2_indicate==0)
   word_par1 <- matrix(0, nrow=f, ncol=k2)
   for(j in 1:k2){
-    word_par1[, j] <- rowSums(matrix(theta3[, j], nrow=f, ncol=k1, byrow=T) * gamma[j, wd])
+    word_par1[-index_zeros, j] <- theta3[z2_indicate[-index_zeros], j] * gamma[j, wd[-index_zeros]]
   }
-  Li1 <- rowSums(word_par1)
+  r <- omega[ID_d]
+  Li1 <- (1-r) * rowSums(word_par1)
   
   #単語ごとに文書固有のトピック尤度を計算
   word_par2 <- burden_fr(theta4, phi, wd, words, k3)
-  Li2 <- rowSums(word_par2$Bur)
+  Li2 <- r * rowSums(word_par2$Bur)
+
+  #潜在確率の計算と潜在変数の生成
+  z_rate1 <- Li2 / (Li1+Li2)
+  Zi1 <- rbinom(f, 1, z_rate1)
+  index_z1 <- which(Zi1==1)
   
-  cbind(wd, Li1, Li2)
+
+  ##多項分布から単語トピックを生成
+  #文書共通のトピックを生成
+  Zi3 <- matrix(0, nrow=f, ncol=k2)
+  z_rate3 <- word_par1 / rowSums(word_par1)
+  Zi3[-index_z1, ] <- rmnom(f-length(index_z1), 1, z_rate3[-index_z1, ])
+  
+  #文書固有のトピックを生成
+  Zi4 <- matrix(0, nrow=f, ncol=k3)
+  z_rate4 <- word_par2$Br
+  Zi4[index_z1, ] <- rmnom(length(index_z1), 1, z_rate4[index_z1, ])
   
   
-  #単語ごとのトピック尤度とトピックの候補を生成
-  word_par <- burden_fr(theta, phi, wd, words, k)   
-  Li01 <- word_par$Bur   #トピックモデルの期待尤度
-  Zi02 <- rmnom(f, 1, word_par$Br)     #多項分布からトピックを生成
-  z02 <- as.numeric(Zi02 %*% 1:k)
+  ##HMMで文章単位のセグメントを生成
+  #文章単位でのトピック頻度行列を作成
+  HMM_data <- matrix(0, nrow=a, ncol=k2)
+  for(i in 1:a){
+    HMM_data[i, ] <- rep(1, length(s_list[[i]])) %*% Zi3[s_list[[i]], , drop=FALSE]
+  }
   
-  #マルコフ推移モデルの尤度と割当確率を逐次的に推定
-  Zi1 <- rep(0, f)
-  z1_rate <- rep(0, f)
-  rf02 <- rep(0, f)
+  #潜在変数ごとに尤度を推定
+  theta_log <- log(t(theta3))
+  LLi0 <- HMM_data %*% theta_log   #対数尤度
+  LLi_max <- apply(LLi0, 1, max)
+  LLi <- exp(LLi0 - LLi_max)   #尤度
   
-  for(j in 2:max_word){
-    
-    #データの設定
-    index <- index_t22[[j]]
-    x0 <- x_vec[index]
-    Li01_obz <- Li01[index, , drop=FALSE]
-    Zi02_obz <-  Zi02[index_t21[[j]], ]
-    
-    #マルコフ切換え確率を推定
-    Li11 <- (1-r[x0+1]) * rowSums(Li01_obz * Zi02_obz)
-    Li12 <- r[x0+1] * rowSums(Li01_obz * (1-Zi02_obz))
-    z1_rate[index] <- Li12 / (Li11+Li12)
-    
-    #ベルヌーイ分布から切換え変数を生成
-    Zi1[index] <- rbinom(length(index), 1, z1_rate[index])
-    index_z1 <- which(Zi1[index]==0)
-    if(length(index_z1)==0) next
-    
-    if(length(index)!=1){
-      Zi02[index, ][index_z1, ] <- Zi02[index_t21[[j]], , drop=FALSE][index_z1, ]
+  #セグメント割当確率の推定とセグメントの生成
+  z_rate2 <- matrix(0, nrow=a, ncol=k1)
+  Zi2 <- matrix(0, nrow=a, ncol=k1)
+  z2_vec <- rep(0, a)
+  rf02 <- matrix(0, nrow=k1, ncol=k1) 
+  
+  for(j in 1:max_time){
+    if(j==1){
+      #セグメントの割当確率
+      LLs <- matrix(theta1, nrow=length(index_t11), ncol=k1, byrow=T) * LLi[index_t11, ]   #重み付き尤度
+      z_rate2[index_t11, ] <- LLs / rowSums(LLs)   #割当確率
+      
+      #多項分布よりセグメントを生成
+      Zi2[index_t11, ] <- rmnom(length(index_t11), 1, z_rate2[index_t11, ])
+      z2_vec[index_t11] <- as.numeric(Zi2[index_t11, ] %*% 1:k1)
+      
+      #混合率のパラメータを更新
+      rf01 <- colSums(Zi2[index_t11, ])
+      
     } else {
-      Zi02[index, ] <- Zi02[index_t21[[j]], ]
+      
+      #セグメントの割当確率
+      index <- index_t22[[j]]
+      LLs <- theta2[z2_vec[index_t21[[j]]], , drop=FALSE] * LLi[index, , drop=FALSE]   #重み付き尤度
+      z_rate2[index, ] <- LLs / rowSums(LLs)   #割当確率
+      
+      #多項分布よりセグメントを生成
+      Zi2[index, ] <- rmnom(length(index), 1, z_rate2[index, ])
+      z2_vec[index] <- as.numeric(Zi2[index, ] %*% 1:k1)
+      
+      #混合率のパラメータを更新
+      rf02 <- rf02 + t(Zi2[index_t21[[j]], , drop=FALSE]) %*% Zi2[index, , drop=FALSE]   #マルコフ推移
     }
   }
-  Zi2 <- Zi02
-  z2_vec <- as.numeric(Zi2 %*% 1:k)
   
+  ##パラメータをサンプリング
+  #ディクレリ分布からHMMの混合率をサンプリング
+  rf11 <- colSums(Zi1[index_t11, ]) + beta01
+  rf12 <- rf02 + alpha01
+  theta1 <- extraDistr::rdirichlet(1, rf11)
+  theta2 <- extraDistr::rdirichlet(k1, rf12)
   
-  ##MH法で混合率の回帰パラメータをサンプリング
-  #データの設定
-  y <- Zi1[-index_t11]
-  x <- x_vec[-index_t11]
-  
-  #betaのサンプリング
-  betad <- c(beta0, beta1)
-  betan <- betad + rw * rnorm(2)   #新しいbetaをランダムウォークでサンプリング
-  
-  #対数尤度と対数事前分布の計算
-  lognew <- loglike(betan[1], betan[2], x, y)
-  logold <- loglike(betad[1], betad[2], x, y)
-  logpnew <- lndMvn(betan, betas, B0)
-  logpold <- lndMvn(betad, betas, B0)
-  
-  
-  #MHサンプリング
-  alpha <- min(1, exp(lognew + logpnew - logold - logpold))
-  if(alpha == "NAN") alpha <- -1
-  
-  #一様乱数を発生
-  u <- runif(1)
-  
-  #u < alphaなら新しいbetaを採択
-  if(u < alpha){
-    beta0 <- betan[1]
-    beta1 <- betan[2]
-    
-    #そうでないならbetaを更新しない
-  } else {
-    beta0 <- betad[1]
-    beta1 <- betad[2]
+  #文書共通のトピック分布のパラメータをサンプリング
+  wf0 <- matrix(0, nrow=k1, ncol=k2)
+  for(j in 1:k1){
+    wf0[j, ] <- colSums(HMM_data * Zi2[, j])
   }
-  
-  #切換え確率の混合率を更新
-  r0 <- exp(beta0) / (1+exp(beta0))
-  r1 <- exp(beta0+beta1) / (1+exp(beta0+beta1))
-  r <- cbind(r0, r1)
-  
-  ##トピックモデルのパラメータをサンプリング
-  #トピック分布thetaをサンプリング
-  wsum0 <- matrix(0, nrow=d, ncol=k)
+  wf <- wf0 + beta01
+  theta3 <- extraDistr::rdirichlet(k1, wf)
+
+  #文書固有のトピック分布のパラメータをサンプリング
+  wsum0 <- matrix(0, nrow=d, ncol=k3)
   for(i in 1:d){
-    wsum0[i, ] <- colSums(Zi2[doc_list[[i]], ])
+    wsum0[i, ] <- rep(1, length(doc_list[[i]])) %*% Zi4[doc_list[[i]], ]
   }
   wsum <- wsum0 + beta01
-  theta <- extraDistr::rdirichlet(d, wsum)
+  theta4 <- extraDistr::rdirichlet(d, wsum)
+  
+  
+  #単語分布gammaをサンプリング
+  gf0 <- matrix(0, nrow=k2, ncol=v)
+  for(j in 1:v){
+    gf0[, j] <- colSums(Zi3[word_list[[j]], , drop=FALSE])
+  }
+  gf <- gf0 + alpha02
+  gamma <- extraDistr::rdirichlet(k2, gf)
   
   #単語分布phiをサンプリング
-  vf0 <- matrix(0, nrow=k, ncol=v)
+  vf0 <- matrix(0, nrow=k2, ncol=v)
   for(j in 1:v){
-    vf0[, j] <- colSums(Zi2[word_list[[j]], , drop=FALSE])
+    vf0[, j] <- colSums(Zi4[word_list[[j]], , drop=FALSE])
   }
-  vf <- vf0 + alpha11
-  phi <- extraDistr::rdirichlet(k, vf)
-  
+  vf <- vf0 + alpha03
+  phi <- extraDistr::rdirichlet(k3, vf)
+
   
   ##パラメータの格納とサンプリング結果の表示
   #サンプリングされたパラメータを格納
