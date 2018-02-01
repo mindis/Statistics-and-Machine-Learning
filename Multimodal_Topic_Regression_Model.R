0a45e5a1b031ad1994880d10e7d9fbb74bd2f4ef
you-chan2 <verticalbird2007@yahoo.co.jp>
2018-02-02 08:54:04 +0900

    【統計モデル】Multimodal Topic Regression Model

--- Multimodal_Topic_Regression_Model.R
+++ Multimodal_Topic_Regression_Model.R
@@ -1,4 +1,4 @@
-#####Multinomial Topic Multinomial Regression Model####
+#####Multinomial Topic Regression Model####
 options(warn=2)
 library(MASS)
 library(lda)
@@ -19,7 +19,7 @@ library(ggplot2)
 dataset <- 2   #データセット数
 k <- 15   #トピック数
 hh <- 2000   #ユーザー数
-page <- 500   #ページ数
+page <- 400   #ページ数
 item <- 1000   #アイテム数
 w1 <- rpois(hh, rgamma(hh, 35, 0.3))   #1人あたいのページ閲覧数
 w2 <- rpois(hh, rgamma(hh, 32, 0.3))   #1人あたりのアイテム購買数
@@ -37,75 +37,84 @@ for(i in 1:hh){
   t_id2 <- c(t_id2, 1:w2[i])
 }
 
+##デザイン行列の設定
+Data <- cbind(rep(1, hh*dataset), rep(0:(dataset-1), rep(hh, dataset)))
+index_logit <- rep(1:hh, dataset)
+index_data <- matrix(1:(hh*dataset), nrow=hh, ncol=dataset)
 
 ##パラメータの設定
-#ディレクリ分布のパラメータ
-alpha01 <- rep(0.2, k)
-alpha02 <- matrix(0.2, nrow=k1, ncol=k1)
-diag(alpha02) <- 2.25
-alpha03 <- rep(0.15, k2)
-alpha11 <- rep(0.1, v)
+for(rp in 1:100){
+  print(rp)
+  #トピック分布のパラメータ
+  alpha01 <- alphat01 <- cbind(mvrnorm(hh, rep(0, k-1), diag(3.5, k-1)), 0)
+  alpha02 <- alphat02 <- cbind(mvrnorm(hh, rep(0, k-1), diag(0.75, k-1)), 0)
+  alpha11 <- rep(0.15, page)
+  alpha12 <- rep(0.1, item)
 
-
-
-for(l in 1:100){
-  print(l)
   #パラメータを生成
-  theta1 <- thetat1 <- extraDistr::rdirichlet(1, alpha01)
-  theta2 <- thetat2 <- extraDistr::rdirichlet(k1, alpha02)
-  theta3 <- thetat3 <- extraDistr::rdirichlet(k1, alpha03)
-  phi0 <- t(extraDistr::rdirichlet(v, rep(0.01, k2))) * 
-    (matrix(extraDistr::rdirichlet(1, rep(2.0, v)), nrow=k2, ncol=v, byrow=T))
+  logit <- Data[, 1]*alpha01[index_logit, ] + Data[, 2]*alpha02[index_logit, ]   #ロジットの計算
+  theta1 <- thetat1 <- exp(logit[index_data[, 1], ]) / rowSums(exp(logit[index_data[, 1], ]))
+  theta2 <- thetat2 <- exp(logit[index_data[, 2], ]) / rowSums(exp(logit[index_data[, 2], ]))
+  phi0 <- t(extraDistr::rdirichlet(page, rep(0.01, k))) * 
+                (matrix(extraDistr::rdirichlet(1, rep(2.0, page)), nrow=k, ncol=page, byrow=T))
   phi <- phit <- phi0 / rowSums(phi0)
+  gamma0 <- t(extraDistr::rdirichlet(item, rep(0.01, k))) * 
+                (matrix(extraDistr::rdirichlet(1, rep(2.0, item)), nrow=k, ncol=item, byrow=T))
+  gamma <- gammat <- gamma0 / rowSums(gamma0)
   
   ##モデルにもとづき単語を生成する
-  WX <- matrix(0, nrow=a, ncol=v)
+  WX1 <- matrix(0, nrow=hh, ncol=page)
+  WX2 <- matrix(0, nrow=hh, ncol=item)
   Z1_list <- list()
   Z2_list <- list()
-  wd_list <- list()
+  wd1_list <- list()
+  wd2_list <- list()
   
-  for(i in 1:a){
-    #文章ごとのセグメントを生成
-    if(t_id[i]==1){
-      z1 <- rmnom(1, 1, theta1)
-      Z1_list[[i]] <- as.numeric(z1 %*% 1:k1)
-    } else {
-      z1 <- rmnom(1, 1, theta2[Z1_list[[i-1]], ])
-      Z1_list[[i]] <- as.numeric(z1 %*% 1:k1)
-    }
+  for(i in 1:hh){
+    #多項分布からトピックを生成
+    z1 <- rmnom(w1[i], 1, theta1[i, ])
+    z1_vec <- as.numeric(z1 %*% 1:k)
+    z2 <- rmnom(w2[i], 1, theta2[i, ])
+    z2_vec <- as.numeric(z2 %*% 1:k)
     
-    #単語ごとにトピックと単語を生成
-    z2 <- rmnom(w[i], 1, theta3[Z1_list[[i]], ])
-    Z2_list[[i]] <- as.numeric(z2 %*% 1:k2)
+    #トピック分布から履歴を生成
+    data1 <- rmnom(w1[i], 1, phi[z1_vec, ])
+    data2 <- rmnom(w2[i], 1, gamma[z2_vec, ])
     
-    #トピックに基づき単語を生成
-    word <- rmnom(w[i], 1, phi[Z2_list[[i]], ])
-    WX[i, ] <- colSums(word)
-    wd_list[[i]] <- as.numeric(word %*% 1:v)
+    #データを格納
+    WX1[i, ] <- colSums(data1)
+    WX2[i, ] <- colSums(data2)
+    Z1_list[[i]] <- z1
+    Z2_list[[i]] <- z2
+    wd1_list[[i]] <- as.numeric(data1 %*% 1:page)
+    wd2_list[[i]] <- as.numeric(data2 %*% 1:item)
   }
-  if(min(colSums(WX)) > 0) break
+  if(min(colSums(WX1)) > 0 & min(colSums(WX2)) > 0) break
 }
-colSums(WX)
-
 
 #リストを変換
-wd <- unlist(wd_list)
+wd1 <- unlist(wd1_list)
+wd2 <- unlist(wd2_list)
 z1 <- unlist(Z1_list)
 z2 <- unlist(Z2_list)
-Data <- matrix(as.numeric(table(1:f, wd)), nrow=f, ncol=v) 
-sparse_data <- as(Data, "CsparseMatrix")
-rm(Data)
+sparse_data1 <- as(WX1, "CsparseMatrix")
+sparse_data2 <- as(WX2, "CsparseMatrix")
 
 
 ##インデックスを作成
-doc_list <- list()
-td_list <- s_list
-word_list <- list()
-for(i in 1:d){doc_list[[i]] <- which(ID_d==i)}
-for(i in 1:v){word_list[[i]] <- which(wd==i)}
+user_list1 <- list()
+user_list2 <- list()
+page_list <- list()
+item_list <- list()
+for(i in 1:hh){
+  user_list1[[i]] <- which(u_id1==i)
+  user_list2[[i]] <- which(u_id2==i)
+}
+for(i in 1:page){page_list[[i]] <- which(wd1==i)}
+for(i in 1:item){item_list[[i]] <- which(wd2==i)}
 
 
-####マルコフ連鎖モンテカルロ法でMHMMトピックモデルを推定####
+####マルコフ連鎖モンテカルロ法でMultinomial Topic Regression Modelを推定####
 ##単語ごとに尤度と負担率を計算する関数
 burden_fr <- function(theta, phi, wd, w, k){
   Bur <-  matrix(0, nrow=length(wd), ncol=k)   #負担係数の格納用
@@ -119,30 +128,6 @@ burden_fr <- function(theta, phi, wd, w, k){
   return(bval)
 }
 
-##観測データの対数尤度と潜在変数zを計算するための関数
-LLobz <- function(Data, phi, r, const, hh, k){
-  
-  #多項分布の対数尤度
-  log_phi <- log(t(phi))
-  LLi <- const + Data %*% log_phi
-  
-  #logsumexpの尤度
-  LLi_max <- matrix(apply(LLi, 1, max), nrow=hh, ncol=k)
-  r_matrix <- matrix(r, nrow=hh, ncol=k, byrow=T)
-  
-  #割当確率のパラメータを設定
-  expl <- exp(LLi - LLi_max)
-  z <- expl / rowSums(expl)   #セグメント割当確率
-  
-  #観測データの対数尤度
-  r_log <- matrix(log(r), nrow=hh, ncol=k, byrow=T)
-  LLosum <- sum(log(rowSums(exp(r_log + LLi))))   #観測データの対数尤度
-  rval <- list(LLob=LLosum, z=z, LL=LLi)
-  return(rval)
-}
-
-
-####HMMトピックモデルのMCMCアルゴリズムの設定####
 ##アルゴリズムの設定
 R <- 10000
 keep <- 2  
@@ -150,121 +135,66 @@ iter <- 0
 burnin <- 1000/keep
 disp <- 10
 
-##パラメータの真値
-theta1 <- thetat1
-theta2 <- thetat2
-theta3 <- thetat3
-phi <- phit
-z1_vec <- z1
 
-
-##MHMTモデルの初期値を設定
-##混合多項分布でセグメント割当を初期化
-const <- lfactorial(w) - rowSums(lfactorial(WX))   #多項分布の密度関数の対数尤度の定数
-
-#パラメータの初期値
-#phiの初期値
-alpha0 <- colSums(WX) / sum(WX) + 0.001
-phi <- extraDistr::rdirichlet(k1, alpha0*v)
-
-#混合率の初期値
-r <- rep(1/k1, k1)
-
-#観測データの対数尤度の初期化
-L <- LLobz(WX, phi, r, const, a, k1)
-LL1 <- L$LLob
-z <- L$z
-
-#更新ステータス
-dl <- 100   #EMステップでの対数尤度の差の初期値
-tol <- 1
-iter <- 0 
-
-##EMアルゴリズムで対数尤度を最大化
-while(abs(dl) >= tol){   #dlがtol以上の場合は繰り返す
-  #Eステップの計算
-  z <- L$z   #潜在変数zの出力
-  
-  #Mステップの計算と最適化
-  #phiの推定
-  df0 <- matrix(0, nrow=k1, ncol=v)
-  for(j in 1:k1){
-    #完全データの対数尤度からphiの推定量を計算
-    phi[j, ] <- colSums(matrix(z[, j], nrow=a, ncol=v) * WX) / sum(z[, j] * w)   #重み付き多項分布の最尤推定
-  }
-  
-  #混合率を推定
-  r <- apply(z, 2, sum) / a
-  
-  #観測データの対数尤度を計算
-  phi[phi==0] <- min(phi[phi > 0])
-  L <- LLobz(WX, phi, r, const, a, k1)
-  LL <- L$LLob   #観測データの対数尤度
-  iter <- iter+1   
-  dl <- LL-LL1
-  LL1 <- LL
-  print(LL)
-}
-
-#初期値を設定
-theta1 <- extraDistr::rdirichlet(1, rep(1, k1))
-alpha <- matrix(0.3, nrow=k1, ncol=k1)
-diag(alpha) <- 1.5
-theta2 <- extraDistr::rdirichlet(k1, alpha)
-theta3 <- extraDistr::rdirichlet(k1, rep(0.3, k2))
-z1_vec <- as.numeric(rmnom(a, 1, z) %*% 1:k1)
-
+##初期値を設定
+alpha1 <- cbind(mvrnorm(hh, rep(0, k-1), diag(1.0, k-1)), 0)
+alpha2 <- cbind(mvrnorm(hh, rep(0, k-1), diag(0.25, k-1)), 0)
+logit <- Data[, 1]*alpha1[index_logit, ] + Data[, 2]*alpha2[index_logit, ]   #ロジットの計算
+theta1 <- exp(logit[index_data[, 1], ]) / rowSums(exp(logit[index_data[, 1], ]))
+theta2 <- exp(logit[index_data[, 2], ]) / rowSums(exp(logit[index_data[, 2], ]))
+phi0 <- extraDistr::rdirichlet(k, colSums(WX1)/sum(WX1)*10) + 0.0001
+phi <- phi0 / rowSums(phi0)
+gamma0 <- extraDistr::rdirichlet(k, colSums(WX2)/sum(WX2)*10) + 0.0001
+gamma <- gamma0 / rowSums(gamma0)
 
 ##事前分布の設定
 #ハイパーパラメータの事前分布
-alpha01 <- 0.1
-alpha02 <- 0.1
-beta01 <- 1
-beta02 <- 1
+alpha01 <- 0.5
+alpha02 <- 0.5
+
+#変量効果の事前分布
+nu <- k-1   #逆ウィシャート分布の自由度
+V <- nu * diag(rep(1, (dataset*k)-dataset))
 
 
 ##パラメータの格納用配列
-THETA1 <- matrix(0, nrow=R/keep, ncol=k1)
-THETA2 <- array(0, dim=c(k1, k1, R/keep))
-THETA3 <- array(0, dim=c(k1, k2, R/keep))
-PHI <- array(0, dim=c(k2, v, R/keep))
-SEG1 <- matrix(0, nrow=a, ncol=k1)
-SEG2 <- matrix(0, nrow=f, ncol=k2)
+ALPHA1 <- array(0, dim=c(hh, k-1, R/keep))
+ALPHA2 <- array(0, dim=c(hh, k-1, R/keep))
+SIGMA <- matrix(0, nrow=R/keep, ncol=(dataset*k)-dataset) 
+PHI <- array(0, dim=c(k, page, R/keep))
+GAMMA <- array(0, dim=c(k, item, R/keep))
+SEG1 <- matrix(0, nrow=f1, ncol=k)
+SEG2 <- matrix(0, nrow=f2, ncol=k)
 storage.mode(SEG1) <- "integer"
 storage.mode(SEG2) <- "integer"
 
-
-##MCMC推定用配列
-max_time <- max(t_id)
-max_word <- max(words)
-index_t11 <- which(t_id==1)
-index_t21 <- list()
-index_t22 <- list()
-for(j in 2:max_word){
-  index_t21[[j]] <- which(t_id==j)-1
-  index_t22[[j]] <- which(t_id==j)
-}
-
 #対数尤度の基準値
-LLst <- sum(WX %*% log(colSums(WX)/f))
+LLst <- sum(WX1 %*% log(colSums(WX1)/f1) + WX2 %*% log(colSums(WX2)/f2))
+
+##パラメータの真値
+alpha1 <- alpha01
+alpha2 <- alpha02
+theta1 <- thetat1
+theta2 <- thetat2
+phi <- phit
+gamma <- gammat
 
 
 ####ギブスサンプリングでHTMモデルのパラメータをサンプリング####
 for(rp in 1:R){
   
-  ##単語ごとに文章単位でトピックを生成
+  ##データセットごとにトピックを生成
   #トピック尤度を計算
-  z1_indicate <- rep(z1_vec, w)
-  word_par <- matrix(0, nrow=f, ncol=k2)
-  for(j in 1:k2){
-    word_par[, j] <- theta3[z1_indicate, j] * phi[j, wd]   #トピック尤度
-  }
-  
+  page_par <- burden_fr(theta1, phi, wd1, w1, k)
+  item_par <- burden_fr(theta2, gamma, wd2, w2, k)
+  page_rate <- page_par$Br
+  item_rate <- item_par$Br
   
   #トピックの割当確率からトピックを生成
-  topic_rate <- word_par / rowSums(word_par)
-  Zi2 <- rmnom(f, 1, topic_rate)
-  z2_vec <- as.numeric(Zi2 %*% 1:k2)
+  Zi1 <- rmnom(f1, 1, page_rate)
+  Zi2 <- rmnom(f2, 1, item_rate)
+  z1_vec <- as.numeric(Zi1 %*% 1:k)
+  z2_vec <- as.numeric(Zi2 %*% 1:k)
   
   
   ##HMMで文章単位のセグメントを生成
