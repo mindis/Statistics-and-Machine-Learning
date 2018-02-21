cb9f63f821bdf6bdc03d53e43fd688e6fde55f3d
you-chan2 <verticalbird2007@yahoo.co.jp>
2018-02-22 08:53:25 +0900

    【統計モデル】HMC多項ロジットモデル

--- HMC_logit_model.R
+++ HMC_logit_model.R
@@ -16,7 +16,7 @@ library(ggplot2)
 
 ####データの発生####
 #データの設定
-hh <- 5000   #サンプル数
+hh <- 10000   #サンプル数
 select <- 10   #選択肢数
 st <- 10   #基準変数
 k1 <- 3   #無条件説明変数数
@@ -63,15 +63,17 @@ Pr <- exp(logit) / rowSums(exp(logit))
 
 #多項分布から応答変数を生成
 y <- rmnom(hh, 1, Pr)
+y_vec <- as.numeric(t(y))
 colSums(y)
+
 
 #####HMCでベイジアン多項ロジットモデルを推定####
 ##Leap Frog法を解く関数
 leapfrog <- function(r, z, D, e, L) {
   leapfrog.step <- function(r, z, e){
-    r2 <- r  - e * D(z, XM, Y, par) / 2
+    r2 <- r  - e * D(z, y_vec, sparse_data, select) / 2
     z2 <- z + e * r2
-    r2 <- r2 - e * D(z2, XM, Y, par) / 2
+    r2 <- r2 - e * D(z2, y_vec, sparse_data, select) / 2
     list(r=r2, z=z2) # 1回の移動後の運動量と座標
   }
   leapfrog.result <- list(r=r, z=z)
@@ -86,27 +88,100 @@ loglike <- function(y, X, beta, hh, select){
   
   #ロジットと確率の計算
   logit <- matrix(X %*% beta, nrow=hh, ncol=select, byrow=T)
-  Pr <- exp(logit) / rowSums(exp(logit))
+  exp_logit <- exp(logit)
+  Pr <- exp_logit / rowSums(exp_logit)
   
   LLi <- rowSums(y * log(Pr))
   LL <- sum(LLi)
-  val <- list(LLi=LLi, LL=LL)
-  return(val)
+  return(LL)
 }
 
 ##多項ロジットモデルの対数尤度の微分関数
-dloglike
+dloglike <- function(beta, y_vec, data, select){
+  #ロジットと確率を計算
+  logit <- matrix(data %*% beta, nrow=hh, ncol=select, byrow=T)
+  exp_logit <- exp(logit)
+  Pr <- exp_logit / rowSums(exp_logit)
+  
+  #ロジットモデルの対数微分関数を定義
+  Pr_vec <- as.numeric(t(Pr))
+  dlogit <- (y_vec - Pr_vec) * data
+  LLd <- -colSums(dlogit)
+  return(LLd)
+}
 
-mean(y[y==1] * Pr[y==1])
-Pr
-(y - Pr) 
-sparse_data
-Data
 
-colSums((as.numeric(t(y)) - as.numeric(t(Pr))) * sparse_data)
+####ハミルトニアンモンテカルロ法でロジットモデルのパラメータをサンプリング####
+##アルゴリズムの設定
+R <- 10000
+keep <- 2
+disp <- 20
+burnin <- 2000/keep
+iter <- 0
+e <- 0.01
+L <- 5
 
-Pr <- matrix(runif(hh*select, 0, 1), nrow=hh, ncol=select)
+##データの設定
+par <- ncol(sparse_data)   #パラメータ数
 
-round(cbind(as.numeric(t(y)) , as.numeric(t(Pr)), as.numeric(t(y)) - as.numeric(t(Pr)),
-            (as.numeric(t(y)) - as.numeric(t(Pr))) * Data), 3)
-beta
+##初期値の設定
+oldbeta <- rep(0, par)
+
+##パラメータの保存用配列
+BETA <- matrix(0, nrow=R/keep, ncol=par)
+ALPHA <- rep(0, R/keep)
+LL <- rep(0, R/keep)
+
+
+####HMCでパラメータをサンプリング####
+for(rp in 1:R){
+  
+  ##リープフロッグ法により新しいパラメータをサンプリング
+  #パラメータの設定
+  rold <- rnorm(par)
+  betad <- oldbeta
+  
+  #リープフロッグ法による1ステップ移動
+  res <- leapfrog(rold, betad, dloglike, e, L)
+  rnew <- res$r
+  betan <- res$z
+  
+  
+  ##HMC法によりパラメータを更新
+  #移動前と移動後のハミルトニアンを計算
+  lognew <- loglike(y, sparse_data, betan, hh, select) 
+  logold <- loglike(y, sparse_data, betad, hh, select)
+  Hnew <- -lognew + sum(rnew^2)/2
+  Hold <- -logold + sum(rold^2)/2
+  
+  #HMC法によりパラメータの採択を決定
+  alpha <- min(1, exp(Hold - Hnew))
+  if(alpha=="NaN") alpha <- -1
+  
+  #一様乱数を発生
+  u <- runif(1)
+  
+  #u < alphaなら新しいbetaを採択
+  if(u < alpha){
+    oldbeta <- betan
+    
+    #そうでないならbetaを更新しない
+  } else {
+    oldbeta <- betad
+  }
+  
+  ##サンプリングを保存する回数ならbetaを書き込む
+  if(rp%%keep==0){
+    mkeep <- rp/keep
+    BETA[mkeep, ] <- oldbeta
+    ALPHA[mkeep] <- alpha
+    LL[mkeep] <- lognew
+    
+    if(rp%%disp==0){
+      print(rp)
+      print(round(alpha, 3))
+      print(round(rbind(betan, betat), 2))
+    }
+  }
+}
+
