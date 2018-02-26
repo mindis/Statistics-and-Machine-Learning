5d0e144676b304124836e15dd4a43bd91b39e8ac
you-chan2 <verticalbird2007@yahoo.co.jp>
2018-02-27 08:52:40 +0900

    【統計モデル】動的ポアソン回帰モデル

--- Dynamic_Poisson_Regression_Model.R
+++ Dynamic_Poisson_Regression_Model.R
@@ -22,7 +22,7 @@ k <- 4   #説明変数数
 for(rp in 1:1000){
   #説明変数のトレンドを生成
   Data0 <- matrix(0, nrow=d, ncol=k)
-  Data0[1, ] <- c(1.5, 0.6, -0.5, -0.5)
+  Data0[1, ] <- c(2.0, 0.6, -0.5, -0.5)
   v0 <- runif(k, 0.015, 0.03)   #システムモデルの分散
   s <- cbind(seq(0.8, 0.2, length=d-1), 
              seq(0.7, 0.4, length=d-1),
@@ -53,9 +53,9 @@ for(rp in 1:1000){
   ##説明変数の動的パラメータを生成
   #初期値を設定
   beta1 <- beta2 <- beta3 <- rep(0, d)
-  beta1[1] <- -1.25   #価格の初期値
-  beta2[1] <- 0.6   #特別陳列の初期値
-  beta3[1] <- 0.6   #チラシ掲載有無の初期値 
+  beta1[1] <- -1.4   #価格の初期値
+  beta2[1] <- 0.8  #特別陳列の初期値
+  beta3[1] <- 0.7   #チラシ掲載有無の初期値 
   
   #システムモデルの分散
   v1 <- v2 <- v3 <- 0.015   
@@ -80,14 +80,15 @@ for(rp in 1:1000){
   beta <- betat <- cbind(beta1, beta2, beta3)
   trend <- Data1[, 1]
   
-  
   ##動的ポアソン回帰モデルから応答変数を生成
   pois_mu <- exp(rowSums(Data * cbind(trend, beta)))   #ポアソン分布の平均
   y <- rpois(d, pois_mu)   #ポアソン分布から応答変数を生成
-  if(max(y[1:(d-500)]) > max(y[(d-500):d]) & max(y) > 30){
+  if(max(y[1:(d-500)]) > max(y[(d-500):d]) & quantile(y, 0.99) > 50){
     break
   }
 }
+
+#生成したデータをプロット
 plot(1:d, y, type="l", xlab="日数", ylab="購買点数", xlim=c(0, d), ylim=c(0, max(y)))
 par(new=T)
 plot(1:d, pois_mu, xlim=c(0, d), ylim=c(0, max(y)), ylab="", xlab="", type="p", pch=4, col=4)
@@ -95,30 +96,92 @@ plot(1:d, pois_mu, xlim=c(0, d), ylim=c(0, max(y)), ylab="", xlab="", type="p", 
 
 ####粒子フィルタで動的ポアソン回帰モデルを推定####
 ##ポアソン回帰モデルの対数尤度関数
-fr <- function(beta, Data, y, y_factorial){
-  lambda <- exp(Data %*% beta)   #リンク関数
+fr <- function(theta, Data, y, y_factorial){
+  lambda <- exp(Data %*% theta)   #リンク関数
   LLi <- y*log(lambda)-lambda - y_factorial
   LL <- sum(LLi)
   return(LL)
 }
 
 ##粒子フィルタの設定
-s <- 10000   #粒子数
-tau <- diag(0.05^2, k)   #システム分散
+s0 <- 5000   #粒子数
+s <- 10000
 LL <- rep(0, d)
 BETA <- array(0, dim=c(s, k, d))
 y_factorial <- lfactorial(y)
 
-##システムモデルのパラメータを更新
-betan <- c(trend[1], beta[1, ]) + mvrnorm(s, rep(0, k), diag(0.25, k))
+##準ニュートン法で初期値を設定
+theta <- rep(0, k)
+target <- 1:100
+res <- optim(theta, fr, Data=Data[target, ], y=y[target], y_factorial=y_factorial[target],
+             method="BFGS", control=list(fnscale=-1))
+beta <- res$par
 
+
+####粒子フィルタの固定パラメータを推定####
+particle_fr <- function(tau, beta, Data, y, k, s){
+
+  #パラメータの設定
+  BETA <- array(0, dim=c(s, k, d))
+  sigma <- abs(diag(tau, k))
+
+  ##システムモデルのパラメータを更新
+  betan <- matrix(beta, nrow=s, ncol=k, byrow=T) + mvrnorm(s, rep(0, k), diag(0.5, k))
+  
+  ##観測モデルの尤度を評価
+  #尤度を評価
+  lambda <- exp(betan[, 1] + rowSums(matrix(Data[1, -1], nrow=s, ncol=k-1, byrow=T) * betan[, -1]))
+  Li <- dpois(y[1], lambda)   #粒子ごとの尤度
+  LL[1] <- sum(Li)   #尤度の和
+  
+  #尤度の負担率に応じてパラメータをリサンプリング
+  w <- Li/sum(Li) 
+  index <- as.numeric(rmnom(1, s, w))
+  resample <- rep(1:s, index)
+  BETA[, , 1] <- betan[resample, ]   #リサンプリングされたパラメータ
+  
+  ##2期目以降を粒子フィルタで逐次的に更新
+  for(i in 2:d){
+    ##システムモデルのパラメータの更新
+    betan <- BETA[, , i-1] + mvrnorm(s, rep(0, k), sigma)
+  
+    ##観測モデルの尤度を評価
+    #ロジットと確率を計算
+    lambda <- exp(betan[, 1] + rowSums(matrix(Data[i, -1], nrow=s, ncol=k-1, byrow=T) * betan[, -1]))
+    Li <- dpois(y[i], lambda)   #粒子ごとの尤度
+    LL[i] <- sum(Li)   #尤度の和
+    
+    #尤度の負担率に応じてパラメータをリサンプリング
+    w <- Li/sum(Li) 
+    index <- as.numeric(rmnom(1, s, w))
+    resample <- rep(1:s, index)
+    BETA[, , i] <- betan[resample, ]   #パラメータをリサンプリング
+  }
+  
+  #対数尤度の和
+  LLs <- sum(log(LL)) - d*log(s)
+  return(LLs)
+}
+
+##Nelder-Mead法でシステムモデルの分散を推定
+tau <- rep(0.025, k)
+res <- optim(tau, particle_fr, beta=beta, Data=Data, y=y, k=k, s=s0,
+             method="Nelder-Mead", control=list(fnscale=-1, maxit=100, trace=TRUE))
+res$value   #最大化された対数尤度
+v <- diag(res$par, k)   #パラメータ推定値
+
+
+####推定された静的パラメータをもとに動的ポアソン回帰モデルを推定####
+##システムモデルのパラメータを更新
+BETA <- array(0, dim=c(s, k, d))
+beta[2] <- -1.25
+betan <- matrix(beta, nrow=s, ncol=k, byrow=T) + mvrnorm(s, rep(0, k), diag(0.1, k))
 
 ##観測モデルの尤度を評価
 #尤度を評価
-lambda <- exp(betan[, 1] + rowSums(matrix(Data[1, ], nrow=s, ncol=k-1, byrow=T) * betan[, -1]))
+lambda <- exp(betan[, 1] + rowSums(matrix(Data[1, -1], nrow=s, ncol=k-1, byrow=T) * betan[, -1]))
 Li <- dpois(y[1], lambda)   #粒子ごとの尤度
 LL[1] <- sum(Li)   #尤度の和
-
 
 #尤度の負担率に応じてパラメータをリサンプリング
 w <- Li/sum(Li) 
@@ -126,15 +189,14 @@ index <- as.numeric(rmnom(1, s, w))
 resample <- rep(1:s, index)
 BETA[, , 1] <- betan[resample, ]   #リサンプリングされたパラメータ
 
-
 ##2期目以降を粒子フィルタで逐次的に更新
 for(i in 2:d){
   ##システムモデルのパラメータの更新
-  betan <- BETA[, , i-1] + mvrnorm(s, rep(0, k), tau)
-
+  betan <- BETA[, , i-1] + mvrnorm(s, rep(0, k), v)
+  
   ##観測モデルの尤度を評価
   #ロジットと確率を計算
-  lambda <- exp(betan[, 1] + rowSums(matrix(Data[i, ], nrow=s, ncol=k-1, byrow=T) * betan[, -1]))
+  lambda <- exp(betan[, 1] + rowSums(matrix(Data[i, -1], nrow=s, ncol=k-1, byrow=T) * betan[, -1]))
   Li <- dpois(y[i], lambda)   #粒子ごとの尤度
   LL[i] <- sum(Li)   #尤度の和
   
@@ -144,14 +206,28 @@ for(i in 2:d){
   resample <- rep(1:s, index)
   BETA[, , i] <- betan[resample, ]   #パラメータをリサンプリング
 }
+
 #対数尤度の和
-LLs <- sum(log(LL)) - d*log(s)
-LLs
-sum(dpois(y, exp(rowSums(Data * cbind(trend, beta))), log=TRUE))
+round(LLs <- sum(log(LL)) - d*log(s), 3)   #粒子フィルタの対数尤度
+sum(dpois(y, exp(rowSums(Data * cbind(trend, betat))), log=TRUE))   #真のパラメータの対数尤度
 
 
-i <- 100
-round(colMeans(BETA[, , i]), 3)
-Data[i, ] %*% c(trend[i], beta[i, ])
-Data[i,] %*% colMeans(BETA[, , i])
+##パラメータの要約値を推定
+theta <- matrix(0, nrow=d, ncol=k)
+for(i in 1:d){
+  theta[i, ] <- colMeans(BETA[, , i])
+}
+
+#パラメータの推移をプロット
+matplot(cbind(trend, theta[, 1]), type="l", col=1:2, lwd=2, xlab="日数", ylab="パラメータ", main="トレンドの推移")
+matplot(cbind(betat[, 1], theta[, 2]), type="l", col=1:2, lwd=2, xlab="日数", ylab="パラメータ", main="価格弾力性の推移")
+matplot(cbind(betat[, 2], theta[, 3]), type="l", col=1:2, lwd=2, xlab="日数", ylab="パラメータ", main="特別陳列の推移")
+matplot(cbind(betat[, 3], theta[, 4]), type="l", col=1:2, lwd=2, xlab="日数", ylab="パラメータ", main="チラシ掲載の推移")
+
+#予測値と観測変数の比較
+pred_lambda <- exp(rowSums(Data * theta))
+plot(1:d, y, type="l", xlab="日数", ylab="購買点数", xlim=c(0, d), ylim=c(0, max(y)))
+par(new=T)
+plot(1:d, pred_lambda, xlim=c(0, d), ylim=c(0, max(y)), ylab="", xlab="", type="p", pch=4, col=2)
+
 
