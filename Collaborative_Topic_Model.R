0cfd00791a05c67f1c5b654de790212eccc9b8a8
you-chan2 <verticalbird2007@yahoo.co.jp>
2018-03-05 08:44:23 +0900

    【統計モデル】Collaborative Topic Model

--- Collaborative_Topic_Model.R
+++ Collaborative_Topic_Model.R
@@ -19,7 +19,7 @@ library(ggplot2)
 k <- 10
 hh <- 1500   #レビュー人数
 item <- 500   #アイテム数
-s <- rpois(hh, rgamma(hh, 6.5, 0.7))   #1人あたりのレビュー数
+s <- rpois(hh, rgamma(hh, 7.5, 0.6))   #1人あたりのレビュー数
 s[s==0] <- ceiling(runif(length(s[s==0]), 1, 5))
 d <- sum(s)   #総文書数
 v <- 1000   #語彙数
@@ -107,7 +107,8 @@ burden_fr <- function(theta, phi, wd, w, k){
   return(bval)
 }
 
-#インデックスの作成
+##インデックスの作成
+#トピックモデルのインデックスを作成
 doc_list <- list()
 word_list <- list()
 doc_ones <- list()
@@ -126,17 +127,36 @@ for(i in 1:v){
   word_ones[[i]] <- rep(1, word_n[i])
 }
 
+#潜在因子モデルのインデックスを作成
+user_list <- list()
+user_n <- rep(0, hh)
+item_list <- list()
+item_n <- rep(0, item)
+for(i in 1:hh){
+  user_list[[i]] <- which(u_id==i)
+  user_n[i] <- length(user_list[[i]])
+}
+for(i in 1:item){
+  item_list[[i]] <- which(u_vec==i)
+  item_n[i] <- length(item_list[[i]])
+}
+
 
 #初期値の設定
 theta1 <- extraDistr::rdirichlet(item, rep(0.5, k))
 phi <- extraDistr::rdirichlet(k, rep(1.0, v))
+theta2 <- extraDistr::rdirichlet(hh, rep(0.5, k))
+sigma <- 0.2   #誤差標準偏差の初期値
+tau1 <- 0.025   #アイテム因子のハイパーパラメータ
+tau2 <- 0.25   #ユーザー因子のハイパーパラメータ
 
 
 #EMアルゴリズムの更新ステータス
 iter <- 1
 dl <- 100   #EMステップでの対数尤度の差の初期値
-tol <- 1
-LLw <- -1000000000   #対数尤度の初期値
+tol <- 0.1
+LLo <- -1000000000   #対数尤度の初期値
+LLw <- c()
 
 ##EMアルゴリズムでパラメータを更新
 while(abs(dl) >= tol){   #dlがtol以上の場合は繰り返す
@@ -154,6 +174,7 @@ while(abs(dl) >= tol){   #dlがtol以上の場合は繰り返す
     wsum[i, ] <- t(z[doc_list[[i]], ]) %*% doc_ones[[i]]
   }
   theta1 <- wsum / doc_n
+  theta1[is.nan(theta1)] <- rep(1/k, k)
   
   #単語分布phiを更新
   vsum <- matrix(0, nrow=k, ncol=v)
@@ -164,15 +185,87 @@ while(abs(dl) >= tol){   #dlがtol以上の場合は繰り返す
   
   
   ##潜在因子モデルでスコア要因を推定
+  ##アイテム因子psiを推定
+  #データの設定
+  x0 <- thetat2[u_id, ]
+  lambda <- diag(tau1, k)
   
+  #アイテムごとに因子行列を最適化
+  for(j in 1:item){
+    
+    #アイテムがない場合は次のアイテムへ
+    if(item_n[j]==0){
+      psi[, j] <- theta1[j, ]
+      next
+    }
+    #アイテムごとにデータを設定
+    index <- item_list[[j]]
+    x <- x0[index, ]
+    y <- score[index]
+    omega <- diag(sigma^2, item_n[j])
+    
+    #psiを正則化最小二乗法で最適化
+    if(item_n[j] > 1){
+      psi[, j] <- as.numeric(solve(t(x) %*% omega %*% x + lambda) %*% (t(x) %*% omega %*% y + lambda %*% theta1[j, ]))
+    } else {
+      psi[, j] <- as.numeric(solve(x %*% omega %*% x + lambda) %*% (x %*% omega %*% y + lambda %*% theta1[j, ]))
+    }
+  }
+  psi[is.nan(psi)] <- 0
   
+  ##ユーザー因子theta2を推定
+  #データの設定
+  x0 <- t(psi)[u_vec, ]
+  lambda <- diag(tau2, k)
   
+  #ユーザーごとに因子行列を最適化
+  for(i in 1:hh){
+    #ユーザーごとにデータを設定
+    index <- user_list[[i]]
+    x <- x0[index, ]
+    y <- score[index]
+    omega <- diag(sigma, user_n[i])
+
+    #theta2を正則化最小二乗法で最適化
+    if(user_n[i] > 1){
+      theta2[i, ] <- as.numeric(solve(t(x) %*% omega %*% x + lambda) %*% t(x) %*% omega %*% y)
+    } else {
+      theta2[i, ] <- as.numeric(solve(x %*% omega %*% x + lambda) %*% x %*% omega %*% y)
+    }
+  }
   
+  ##誤差sigmaを更新
+  mu <- rowSums(theta2[u_id, ] * t(psi)[u_vec, ])
+  er <- score - mu
+  sigma <- sum(er^2) / (d-1)
+
+  
+  ##EMアルゴリズムの更新
   #対数尤度の計算
-  LLS <- sum(log(rowSums(par$Bur)))
+  LLS1 <- sum(log(rowSums(par$Bur)))   #トピックモデルの対数尤度
+  LLS2 <- -sum(dnorm(score, mu, sqrt(sigma), log=TRUE))   #潜在因子モデルの対数尤度
+  LLS <- LLS1 + LLS2
+  
+  #収束判定
   iter <- iter+1
   dl <- LLS-LLo
   LLo <- LLS
   LLw <- c(LLw, LLo)
   print(LLo)
 }
+
+#####推定結果の要約#####
+##パラメータ推定値
+round(cbind(par$Br, Z %*% 1:k), 3)   #生成トピック
+round(cbind(theta1, thetat1), 2)   #トピック分布
+round(cbind(t(phi), t(phit)), 3)   #単語分布
+round(data.frame(n=user_n, x=theta2, y=thetat2), 2)   #ユーザー因子
+round(data.frame(n=item_n, x=t(psi), y=t(psit)), 2)   #アイテム因子
+
+##スコアの予測値
+pred_mu0 <- theta2 %*% psi
+pred_mu1 <- thetat2 %*% psit
+round(cbind(pred_mu0[, 1:10], pred_mu1[, 1:10]), 2)
+
+
+
