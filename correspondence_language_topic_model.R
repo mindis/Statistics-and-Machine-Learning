a51ebc08b56d0c9820d619fc71ba9d32918c6d1a
you-chan2 <verticalbird2007@yahoo.co.jp>
2018-03-06 08:43:48 +0900

    【統計モデル】対応言語トピックモデル

--- correspondence_language_topic_model.R
+++ correspondence_language_topic_model.R
@@ -50,6 +50,7 @@ beta <- betat <- matrix(rgamma(k1*k2, 0.4, 0.8), nrow=k2, ncol=k1)
 phi <- phit <- extraDistr::rdirichlet(k1, beta01)
 psi <- psit <- extraDistr::rdirichlet(k2, beta02)
 
+
 #逐次的にトピックと単語を生成
 WX1 <- matrix(0, nrow=f0, ncol=v1)
 WX2 <- matrix(0, nrow=f0, ncol=v2)
@@ -115,11 +116,55 @@ burden_fr <- function(theta, phi, wd, w, k){
   return(bval)
 }
 
+##アルゴリズムの設定
+R <- 5000
+keep <- 2  
+iter <- 0
+burnin <- 1000/keep
+disp <- 10
+
+##インデックスを作成
+doc_list <- list()
+doc_vec <- list()
+w_list1 <- list()
+w_vec1 <- list()
+w_list2 <- list()
+w_vec2 <- list()
+pair_list <- list()
+pair_vec <- list()
+
+for(i in 1:d){
+  doc_list[[i]] <- which(w_id==i)
+  doc_vec[[i]] <- rep(1, length(doc_list[[i]]))
+}
+for(j in 1:v1){
+  w_list1[[j]] <- which(word_vec1==j)
+  w_vec1[[j]] <- rep(1, length(w_list1[[j]]))
+}
+for(j in 1:v2){
+  w_list2[[j]] <- which(word_vec2==j)
+  w_vec2[[j]] <- rep(1, length(w_list2[[j]]))
+}
+for(i in 1:f0){
+  if(i%%1000==0){print(i)}
+  pair_list[[i]] <- which(w_id1==i)
+  pair_vec[[i]] <- rep(1, length(pair_list[[i]]))
+}
+
+##ペアIDを作成
+pair_id_list1 <- list()
+for(i in 1:f0){
+  pair_id_list1[[i]] <- rep(pair_list[[i]], s2[i])
+}
+pair_id1 <- unlist(pair_id_list1)
+pair_id2 <- rep(1:f2, rep(s1, s2))
+
+
 ##パラメータの事前分布
-alpha01 <- 10
+alpha01 <- 1
 alpha02 <- 1
 beta01 <- 0.1
-beta02 <- 0.0001
+beta02 <- 0.1
 
 ##パラメータの真値
 theta1 <- thetat1
@@ -127,6 +172,121 @@ theta2 <- thetat2
 phi <- phit
 psi <- psit
 
+##パラメータの初期値
+theta1 <- extraDistr::rdirichlet(d, rep(1, k1))
+theta2 <- extraDistr::rdirichlet(k1, rep(1, k2))
+phi <- extraDistr::rdirichlet(k1, rep(1, v1))
+psi <- extraDistr::rdirichlet(k2, rep(1, v2))
 
-par <- burden_fr(theta1, phi, word_vec1, w_id, k1)
-round(par$Br, 2)
+##パラメータの格納用配列
+THETA1 <- array(0, dim=c(d, k1, R/keep))
+THETA2 <- array(0, dim=c(k1, k2, R/keep))
+PHI <- array(0, dim=c(k1, v1, R/keep))
+PSI <- array(0, dim=c(k2, v2, R/keep))
+SEG1 <- matrix(0, nrow=f1, ncol=k1)
+SEG2 <- matrix(0, nrow=f2, ncol=k2)
+LLho <- rep(0, R/keep)
+
+##対数尤度の基準値
+LLst1 <- sum(WX1 %*% log((colSums(WX1)+0.1) / (sum(WX1)+0.1*v1)))
+LLst2 <- sum(WX2 %*% log((colSums(WX2)+0.1) / (sum(WX2)+0.1*v2)))
+LLst <- LLst1 + LLst2
+
+
+####マルコフ連鎖モンテカルロ法でパラメータをサンプリング####
+for(rp in 1:R){
+  
+  ##名詞トピックをサンプリング
+  #潜在トピックのパラメータを推定
+  par1 <- burden_fr(theta1, phi, word_vec1, w_id, k1)
+  z1_rate <- par1$Br
+  
+  #多項分布よりトピックをサンプリング
+  Zi1 <- rmnom(f1, 1, z1_rate)
+  z1_vec <- as.numeric(Zi1 %*% 1:k1)
+  Zi1_t <- t(Zi1)
+  
+  
+  ##パラメータを更新
+  #トピック分布のパラメータを更新
+  wsum0 <- matrix(0, nrow=d, ncol=k1)
+  for(i in 1:d){
+    wsum0[i, ] <- Zi1_t[, doc_list[[i]]] %*% doc_vec[[i]]
+  }
+  wsum <- wsum0 + alpha01   #ディリクレ分布のパラメータ
+  theta1 <- extraDistr::rdirichlet(d, wsum)   #ディリクレ分布からパラメータをサンプリング
+  
+  #単語分布のパラメータを更新
+  vsum0 <- matrix(0, nrow=k1, ncol=v1)
+  for(j in 1:v1){
+    vsum0[, j] <- Zi1_t[, w_list1[[j]], drop=FALSE] %*% w_vec1[[j]]
+  }
+  vsum <- vsum0 + beta01   #ディリクレ分布のパラメータ
+  phi <- extraDistr::rdirichlet(k1, vsum)   #ディリクレ分布からパラメータをサンプリング
+  
+  
+  ##動詞のトピックをサンプリング
+  #生成した名詞トピックからトピック分布を推定
+  topic_par <- matrix(0, nrow=f0, ncol=k2)
+  par2 <- t(t(log(theta2))[z1_vec, ])
+  
+  for(i in 1:f0){
+    topic_par[i, ] <- par2[, pair_list[[i]], drop=FALSE] %*% pair_vec[[i]]   #トピック分布の対数尤度
+  }
+  word_par <- t(log(psi))[word_vec2, ]   #単語出現率の対数尤度
+  LLi0 <- topic_par[w_id2, ] + word_par   #ペアごとの対数尤度
+  LLi <- exp(LLi0 - rowMaxs(LLi0))   #尤度に変換
+  
+  #多項分布より潜在変数zをサンプリング
+  z2_rate <- LLi / rowSums(LLi)   #潜在変数zの割当確率
+  Zi2 <- rmnom(f2, 1, z2_rate)   #多項分布より潜在変数zをサンプリング
+  z2_vec <- as.numeric(Zi2 %*% 1:k2)
+  Zi2_t <- t(Zi2)
+  
+  
+  ##パラメータを更新
+  #トピック分布のパラメータを更新
+  pair_sums0 <- t(Zi2[pair_id2, ]) %*% Zi1[pair_id1, ]
+  pair_sums <- pair_sums0 + alpha02   #ディリクレ分布のパラメータ
+  theta2 <- extraDistr::rdirichlet(k2, pair_sums)   #ディリクレ分布からパラメータをサンプリング
+  
+  #単語分布のパラメータを更新
+  tsum0 <- matrix(0, nrow=k2, ncol=v2)
+  for(j in 1:v2){
+    tsum0[, j] <- Zi2_t[, w_list2[[j]], drop=FALSE] %*% w_vec2[[j]]
+  }
+  tsum <- tsum0 + beta02   #ディリクレ分布のパラメータ
+  psi <- extraDistr::rdirichlet(k2, tsum)   #ディリクレ分布からパラメータをサンプリング
+
+  
+  ##パラメータの格納とサンプリング結果の表示
+  #サンプリングされたパラメータを格納
+  if(rp%%keep==0){
+    #サンプリング結果の格納
+    mkeep <- rp/keep
+    THETA1[, , mkeep] <- theta1
+    THETA2[, , mkeep] <- theta2
+    PHI[, , mkeep] <- phi
+    PSI[, , mkeep] <- psi
+    
+    #トピック割当はバーンイン期間を超えたら格納する
+    if(rp%%keep==0 & rp >= burnin){
+      SEG1 <- SEG1 + Zi1
+      SEG2 <- SEG2 + Zi2
+    }
+    
+    #サンプリング結果を確認
+    if(rp%%disp==0){
+      LL1 <- sum(log(rowSums(par1$Bur)))
+      LL2 <- sum(log(rowSums(exp(word_par) * Zi2)))
+      LL <- LL1 + LL2   #対数尤度
+      print(rp)
+      print(c(LL, LLst))
+      print(round(rbind(theta1[1:5, ], thetat1[1:5, ]), 3))
+      print(round(cbind(psi[, 1:10], psit[, 1:10]), 3))
+    }
+  }
+}
+
+
+
