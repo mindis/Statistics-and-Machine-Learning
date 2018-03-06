e4b4e94b12e70c372a866e1823897ee656bf05cf
you-chan2 <verticalbird2007@yahoo.co.jp>
2018-03-07 08:40:48 +0900

    【統計モデル】対応言語トピックモデル

--- correspondence_language_topic_model.R
+++ correspondence_language_topic_model.R
@@ -16,7 +16,7 @@ library(ggplot2)
 
 ####データの発生####
 ##文書の設定
-k1 <- 15   #名詞のトピック数
+k1 <- 10   #名詞のトピック数
 k2 <- 15   #動詞と形容詞のトピック数
 d <- 2000   #文書数
 v1 <- 1000   #名詞の語彙数
@@ -46,10 +46,9 @@ beta02 <- rep(0.1, v2)   #動詞の単語分布のパラメータ
 
 #パラメータを生成
 theta1 <- thetat1 <- extraDistr::rdirichlet(d, alpha01)
-beta <- betat <- matrix(rgamma(k1*k2, 0.4, 0.8), nrow=k2, ncol=k1)
+beta <- betat <- matrix(rgamma(k1*k2, 0.25, 0.5), nrow=k1, ncol=k2)
 phi <- phit <- extraDistr::rdirichlet(k1, beta01)
 psi <- psit <- extraDistr::rdirichlet(k2, beta02)
-
 
 #逐次的にトピックと単語を生成
 WX1 <- matrix(0, nrow=f0, ncol=v1)
@@ -75,7 +74,7 @@ for(i in 1:f0){
   pr2 <- U / sum(U)
   z2 <- rmnom(s2[i], 1, pr2)
   z2_vec <- as.numeric(z2 %*% 1:k2)
-  
+
   #トピックにもとづき単語を生成
   word1 <- rmnom(s1[i], 1, phi[z1_vec, ])
   word2 <- rmnom(s2[i], 1, psi[z2_vec, ])
@@ -109,7 +108,7 @@ theta2 <- thetat2 <- extraDistr::rdirichlet(k2, Z_sums + 0.01)
 ##単語ごとに尤度と負担率を計算する関数
 burden_fr <- function(theta, phi, wd, w, k){
   #負担係数を計算
-  Bur <- theta1[w, ] * t(phi)[wd, ]   #尤度
+  Bur <- theta[w, ] * t(phi)[wd, ]   #尤度
   Br <- Bur / rowSums(Bur)   #負担率
   r <- colSums(Br) / sum(Br)   #混合率
   bval <- list(Br=Br, Bur=Bur, r=r)
@@ -174,7 +173,7 @@ psi <- psit
 
 ##パラメータの初期値
 theta1 <- extraDistr::rdirichlet(d, rep(1, k1))
-theta2 <- extraDistr::rdirichlet(k1, rep(1, k2))
+theta2 <- extraDistr::rdirichlet(k2, rep(1, k1))
 phi <- extraDistr::rdirichlet(k1, rep(1, v1))
 psi <- extraDistr::rdirichlet(k2, rep(1, v2))
 
@@ -204,14 +203,14 @@ for(rp in 1:R){
   #多項分布よりトピックをサンプリング
   Zi1 <- rmnom(f1, 1, z1_rate)
   z1_vec <- as.numeric(Zi1 %*% 1:k1)
-  Zi1_t <- t(Zi1)
+  Zi1_T <- t(Zi1)
   
   
   ##パラメータを更新
   #トピック分布のパラメータを更新
   wsum0 <- matrix(0, nrow=d, ncol=k1)
   for(i in 1:d){
-    wsum0[i, ] <- Zi1_t[, doc_list[[i]]] %*% doc_vec[[i]]
+    wsum0[i, ] <- Zi1_T[, doc_list[[i]]] %*% doc_vec[[i]]
   }
   wsum <- wsum0 + alpha01   #ディリクレ分布のパラメータ
   theta1 <- extraDistr::rdirichlet(d, wsum)   #ディリクレ分布からパラメータをサンプリング
@@ -219,7 +218,7 @@ for(rp in 1:R){
   #単語分布のパラメータを更新
   vsum0 <- matrix(0, nrow=k1, ncol=v1)
   for(j in 1:v1){
-    vsum0[, j] <- Zi1_t[, w_list1[[j]], drop=FALSE] %*% w_vec1[[j]]
+    vsum0[, j] <- Zi1_T[, w_list1[[j]], drop=FALSE] %*% w_vec1[[j]]
   }
   vsum <- vsum0 + beta01   #ディリクレ分布のパラメータ
   phi <- extraDistr::rdirichlet(k1, vsum)   #ディリクレ分布からパラメータをサンプリング
@@ -229,10 +228,10 @@ for(rp in 1:R){
   #生成した名詞トピックからトピック分布を推定
   topic_par <- matrix(0, nrow=f0, ncol=k2)
   par2 <- t(t(log(theta2))[z1_vec, ])
-  
   for(i in 1:f0){
     topic_par[i, ] <- par2[, pair_list[[i]], drop=FALSE] %*% pair_vec[[i]]   #トピック分布の対数尤度
   }
+  
   word_par <- t(log(psi))[word_vec2, ]   #単語出現率の対数尤度
   LLi0 <- topic_par[w_id2, ] + word_par   #ペアごとの対数尤度
   LLi <- exp(LLi0 - rowMaxs(LLi0))   #尤度に変換
@@ -241,19 +240,20 @@ for(rp in 1:R){
   z2_rate <- LLi / rowSums(LLi)   #潜在変数zの割当確率
   Zi2 <- rmnom(f2, 1, z2_rate)   #多項分布より潜在変数zをサンプリング
   z2_vec <- as.numeric(Zi2 %*% 1:k2)
-  Zi2_t <- t(Zi2)
+  Zi2_T <- t(Zi2)
   
   
   ##パラメータを更新
   #トピック分布のパラメータを更新
-  pair_sums0 <- t(Zi2[pair_id2, ]) %*% Zi1[pair_id1, ]
+  pair_sums0 <- Zi2_T[, pair_id2] %*% Zi1[pair_id1, ]
   pair_sums <- pair_sums0 + alpha02   #ディリクレ分布のパラメータ
   theta2 <- extraDistr::rdirichlet(k2, pair_sums)   #ディリクレ分布からパラメータをサンプリング
+  
   
   #単語分布のパラメータを更新
   tsum0 <- matrix(0, nrow=k2, ncol=v2)
   for(j in 1:v2){
-    tsum0[, j] <- Zi2_t[, w_list2[[j]], drop=FALSE] %*% w_vec2[[j]]
+    tsum0[, j] <- Zi2_T[, w_list2[[j]], drop=FALSE] %*% w_vec2[[j]]
   }
   tsum <- tsum0 + beta02   #ディリクレ分布のパラメータ
   psi <- extraDistr::rdirichlet(k2, tsum)   #ディリクレ分布からパラメータをサンプリング
@@ -281,12 +281,80 @@ for(rp in 1:R){
       LL2 <- sum(log(rowSums(exp(word_par) * Zi2)))
       LL <- LL1 + LL2   #対数尤度
       print(rp)
-      print(c(LL, LLst))
+      print(c(LL, LLst, LL1, LLst1, LL2, LLst2))
       print(round(rbind(theta1[1:5, ], thetat1[1:5, ]), 3))
       print(round(cbind(psi[, 1:10], psit[, 1:10]), 3))
     }
   }
 }
+
+####サンプリング結果の可視化と要約####
+burnin <- 1000/keep   #バーンイン期間
+RS <- R/keep
+
+##サンプリング結果の可視化
+#名詞のトピック分布の可視化
+matplot(t(THETA1[1, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(THETA1[2, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(THETA1[3, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(THETA1[4, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(THETA1[5, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+
+#名詞の単語分布の可視化
+matplot(t(PHI[1, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(PHI[2, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(PHI[3, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(PHI[4, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(PHI[5, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+
+#下位階層のトピック分布の可視化
+matplot(t(THETA2[1, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(THETA2[3, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(THETA2[5, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(THETA2[10, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(THETA2[15, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+        
+#下位階層の単語分布の可視化
+matplot(t(PSI[1, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(PSI[2, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(PSI[3, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(PSI[4, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(PSI[5, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+matplot(t(PSI[6, , ]), type="l", xlab="サンプリング数", ylab="パラメータ")
+
+##サンプリング結果の要約推定量
+#トピック割当の事後分布の要約
+seg_rate1 <- SEG1 / rowSums(SEG1)
+seg_rate2 <- SEG2 / rowSums(SEG2)
+cbind(Z1 %*% 1:k1, round(SEG1 / rowSums(SEG1), 3))   #名詞トピックの割当
+cbind(Z2 %*% 1:k2, round(SEG2 / rowSums(SEG2), 3))   #動詞トピックの割当
+
+#動詞と名詞トピックの関連
+word_theta0 <- matrix(0, nrow=v2, ncol=k1)
+for(j in 1:k1){
+  word_theta0[, j] <- tapply(seg_rate1[pair_id1, j], word_vec2[pair_id2], sum)
+}
+round(word_theta <- word_theta0 / rowSums(word_theta0), 3)
+
+
+#名詞トピック分布の事後推定量
+topic_mu1 <- apply(THETA1[, , burnin:(R/keep)], c(1, 2), mean)   #トピック分布の事後平均
+round(cbind(topic_mu1, thetat1), 3)
+round(topic_sd1 <- apply(THETA1[, , burnin:(R/keep)], c(1, 2), sd), 3)   #トピック分布の事後標準偏差
+
+#単語割当の事後推定量
+word_mu1 <- apply(PHI[, , burnin:(R/keep)], c(1, 2), mean)   #単語の出現率の事後平均
+round(t(rbind(word_mu1, phit)), 3)
+
+#動詞トピック分布の事後推定量
+topic_mu2 <- apply(THETA2[, , burnin:(R/keep)], c(1, 2), mean)   #トピック分布の事後平均
+round(rbind(topic_mu2, thetat2), 3)
+round(topic_sd2 <- apply(THETA2[, , burnin:(R/keep)], c(1, 2), sd), 3)   #トピック分布の事後標準偏差
+
+#下位階層の単語出現確率の事後推定量
+word_mu2 <- apply(PSI[, , burnin:(R/keep)], c(1, 2), mean)   #単語の出現率の事後平均
+round(cbind(t(word_mu2), t(psit)), 3)
+
 
 
 
