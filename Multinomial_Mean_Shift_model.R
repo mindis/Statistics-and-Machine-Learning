131276de0eae1081f0d8025ff19e5a3d95d21319
you-chan2 <verticalbird2007@yahoo.co.jp>
2018-03-16 08:52:59 +0900

    【統計モデル】Multinomial Mean Shift Model

--- Multinomial_Mean_Shift_model.R
+++ Multinomial_Mean_Shift_model.R
@@ -21,7 +21,7 @@ library(ggplot2)
 ####データの発生####
 ##データの設定
 d <- 1000   #文書数
-v <- 500   #語彙数
+v <- 300   #語彙数
 a <- rpois(d, rgamma(d, 15, 0.7))   #区切り文章数
 f1 <- sum(a)   #総文章数
 w <- rtpois(f1, 8, 5, Inf)   #区切り文章ごとの単語数
@@ -78,12 +78,37 @@ theta_all <- do.call(rbind, theta2_list)
 rm(theta1_list); rm(theta2_list)
 
 
+####粒子フィルタでMultinomial Mean Shift modelを推定####
+
+
+##粒子フィルタの設定
+s <- 3000   #粒子数
+alpha1 <- 0.1
+beta1 <- rep(1/v, v)
+
+#パラメータを生成
+par <- WX[1, ]
+beta0 <- (par + alpha1) / sum(par + alpha1)
+
+#尤度を更新
+Li0 <- as.numeric(WX[2, ] %*% t(log(beta0)))
+Li1 <- as.numeric(WX[2, ] %*% t(log(beta1)))
+Li <- cbind(Li0, Li1)
+
+#パラメータをリサンプリング
+resample_par <- exp(Li0 + Li1 - max(Li0 + Li1))
+resample_par / sum(resample_par)
 
 
 
+#潜在変数zの割当確率を更新
+z_par <- exp(Li - rowMaxs(Li))
+z_rate <- z_par / rowSums(z_par)
+z_rate
+rbinom(s, z_rate[, 2])
 
 
-
-
+WX[2, ] %*% log((WX[1, ] + alpha1) / sum(WX[1, ] + alpha1))
+WX[2, ] %*% log(rep(alpha1, v) / (v * alpha1))
 
 
