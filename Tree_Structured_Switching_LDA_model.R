88e961462421b17f49ec5ce9005c3f5cc14029f2
you-chan2 <verticalbird2007@yahoo.co.jp>
2018-07-05 08:53:10 +0900

    【統計モデル】Tree Structured Switching LDA model

--- Tree_Structured_Switching_LDA_model.R
+++ Tree_Structured_Switching_LDA_model.R
@@ -90,7 +90,7 @@ alpha13 <- c(rep(0.001, v1+v2), rep(0.1, v3))
 alpha2 <- rep(1.5, s)
 
 #木構造トピック分布の事前分布
-alpha31 <- c(18.0, 15.0)   #通過確率の事前分布
+alpha31 <- c(18.5, 15.0)   #通過確率の事前分布
 alpha32 <- rep(0.2, max(c(k12, k22)))   #ノード選択確率の事前分布
 
 ###すべての単語が出現するまでデータの生成を続ける
@@ -108,7 +108,7 @@ for(rp in 1:1000){
   for(j in 1:k21){
     phi22[[j]] <- extraDistr::rdirichlet(k22[j], alpha13)
   }
-  phit12 <- phi12; phit21 <- phi21
+  phit12 <- phi12; phit22 <- phi22
   
   #ディリクレ分布からスイッチング変数を生成
   lambda <- lambdat <- extraDistr::rdirichlet(hh, alpha2)
@@ -175,7 +175,6 @@ for(rp in 1:1000){
       z1[index_y2, 1] <- z11_vec
       
       #通過変数を生成
-      
       g1[index_y2] <- rbinom(length(index_y2), 1, gamma1[u_index, ][z1[, 1]])
       
       #下位階層のトピックを生成
@@ -241,3 +240,178 @@ for(rp in 1:1000){
   }
   if(min(colSums(WX)) > 0) break   #break条件
 }
+
+##リストを変換
+wd <- unlist(wd_list)
+y <- do.call(rbind, y_list)
+Z0 <- unlist(Z0_list)
+Z1 <- do.call(rbind, Z1_list)
+Z2 <- do.call(rbind, Z2_list)
+G <- do.call(rbind, G_list)
+sparse_data <- sparseMatrix(i=1:f, wd, x=rep(1, f), dims=c(f, v))
+sparse_data_T <- t(sparse_data)
+rm(wd_list); rm(y_list); rm(Z0_list); rm(Z1_list); rm(Z2_list); rm(G_list)
+gc(); gc()
+
+
+####マルコフ連鎖モンテカルロ法でTree Structured Switching LDA modelを推定####
+##単語ごとに尤度と負担率を計算する関数
+burden_fr <- function(theta, phi, wd, w, k){
+  #負担係数を計算
+  Bur <- theta[w, ] * t(phi)[wd, ]   #尤度
+  Br <- Bur / rowSums(Bur)   #負担率
+  r <- colSums(Br) / sum(Br)   #混合率
+  bval <- list(Br=Br, Bur=Bur, r=r)
+  return(bval)
+}
+
+##アルゴリズムの設定
+R <- 5000
+keep <- 2  
+iter <- 0
+burnin <- 1000
+disp <- 10
+
+##事前分布の設定
+alpha01 <- 0.1
+alpha02 <- 0.1
+beta01 <- 1.0
+beta02 <- 0.5
+
+##パラメータの真値
+lambda <- lambdat
+gamma1 <- gammat1
+gamma2 <- gammat2
+theta0 <- thetat0
+theta11 <- thetat11
+theta12 <- thetat12
+theta21 <- thetat21
+theta22 <- thetat22
+phi0 <- phit0
+phi11 <- phit11
+phi12 <- phit12
+phi21 <- phit21
+phi22 <- phit22
+
+##データのインデックスを設定
+vec0 <- rep(1, k0); vec11 <- rep(1, k11); vec21 <- rep(1, k21)
+
+
+####ギブスサンプリングでパラメータをサンプリング####
+##期待対数尤度を設定
+#一般語の期待対数尤度
+Lho0 <- matrix(theta0, nrow=f, ncol=k0, byrow=T) * t(phi0)[wd, ]  
+Lis0 <- as.numeric(Lho0 %*% vec0)
+
+#ユーザー関連語の期待対数尤度
+Lho11 <- theta11[u_id, ] * t(phi11)[wd, ]
+Lis11 <- as.numeric(((1-gamma1[u_id, ]) * Lho11) %*% vec11)
+Lho12 <- list(); Lis12 <- matrix(0, nrow=f, ncol=k11)
+for(j in 1:k11){
+  Lho12[[j]] <- theta12[[j]][u_id, ] * t(phi12[[j]])[wd, ]
+  Lis12[, j] <- gamma1[u_id, j] * as.numeric(Lho12[[j]] %*% rep(1, k12[j]))
+}
+
+#アイテム関連語の期待対数尤度
+Lho21 <- theta21[i_id, ] * t(phi21)[wd, ]
+Lis21 <- as.numeric(((1-gamma2[i_id, ]) * Lho21) %*% vec21)
+Lho22 <- list(); Lis22 <- matrix(0, nrow=f, ncol=k21)
+for(j in 1:k21){
+  Lho22[[j]] <- theta22[[j]][i_id, ] * t(phi22[[j]])[wd, ]
+  Lis22[, j] <- gamma2[i_id, j] * as.numeric(Lho22[[j]] %*% rep(1, k22[j]))
+}
+
+##多項分布からスイッチング変数をサンプリング
+#スイッチング変数の割当確率
+Lis12_1 <- theta11[u_id, ] * Lis12
+Lis22_1 <- theta21[i_id, ] * Lis22
+Lis1 <- Lis11 + as.numeric(Lis12_1 %*% vec11)
+Lis2 <- Lis21 + as.numeric(Lis22_1 %*% vec21)
+Lis_par <- lambda[u_id, ] * cbind(Lis0, Lis1, Lis2)
+y_rate <- Lis_par / as.numeric(Lis_par %*% rep(1, s))   #潜在変数の割当確率
+
+#スイッチング変数をサンプリング
+yi <- rmnom(f, 1, y_rate)   #多項分布からサンプリング
+y_vec <- as.numeric(yi %*% 1:s)
+index_user <- which(yi[, 2]==1)
+
+
+##ユーザートピックの木構造を生成
+##上位階層のノードを生成
+#トピックノードの割当確率
+par <- ((1-gamma1[u_id[index_user], ]) * Lho11[index_user, ]) + Lis12_1[index_user, ]
+par_rate <- par / as.numeric(par %*% vec11)   #潜在変数の割当確率
+
+#多項分布からトピックノードを生成
+Zi11 <- matrix(0, nrow=f, ncol=k11)
+Zi11[index_user, ] <- rmnom(length(index_user), 1, par_rate)
+z11_vec <- as.numeric(Zi11 %*% 1:k11)
+Zi11_T <- t(Zi11)
+
+##通過変数をサンプリング
+#通過確率を設定
+g1_rate <- Lis12_1[index_user, ] / par
+g1_rate[is.nan(g1_rate)] <- 0
+
+#ベルヌーイ分布から通過変数をサンプリング
+g1 <- rep(0, f)
+g1[index_user] <- rbinom(length(index_user), 1, as.numeric((g1_rate * Zi11[index_user, ]) %*% vec11))
+
+##下位階層のノードを生成
+Zi12_T_list <- list()
+z12_vec <- matrix(0, nrow=f, ncol=k11)
+
+for(j in 1:k11){
+  #トピックノードの割当確率
+  index_node <- which(Zi11[, j]==1 & g1==1)
+  par <- Lho12[[j]][index_node, ]
+  par_rate <- par / as.numeric(par%*% rep(1, ncol(par)))   #潜在変数の割当確率
+  
+  #多項分布からトピックノードを生成
+  Zi12 <- matrix(0, nrow=f, ncol=k12[j])
+  Zi12[index_node, ] <- rmnom(length(index_node), 1, par_rate)  
+  Zi12_T_list[[j]] <- t(Zi12)
+  z12_vec[, j] <- as.numeric(Zi12 %*% 1:k12[j])
+}
+
+
+##アイテムトピックの木構造を生成
+##上位階層のノードを生成
+#トピックノードの割当確率
+par <- ((1-gamma1[u_id[index_user], ]) * Lho11[index_user, ]) + Lis12_1[index_user, ]
+par_rate <- par / as.numeric(par %*% vec11)   #潜在変数の割当確率
+
+#多項分布からトピックノードを生成
+Zi11 <- matrix(0, nrow=f, ncol=k11)
+Zi11[index_user, ] <- rmnom(length(index_user), 1, par_rate)
+z11_vec <- as.numeric(Zi11 %*% 1:k11)
+Zi11_T <- t(Zi11)
+
+##通過変数をサンプリング
+#通過確率を設定
+g1_rate <- Lis12_1[index_user, ] / par
+g1_rate[is.nan(g1_rate)] <- 0
+
+#ベルヌーイ分布から通過変数をサンプリング
+g1 <- rep(0, f)
+g1[index_user] <- rbinom(length(index_user), 1, as.numeric((g1_rate * Zi11[index_user, ]) %*% vec11))
+
+##下位階層のノードを生成
+Zi12_T_list <- list()
+z12_vec <- matrix(0, nrow=f, ncol=k11)
+
+for(j in 1:k11){
+  #トピックノードの割当確率
+  index_node <- which(Zi11[, j]==1 & g1==1)
+  par <- Lho12[[j]][index_node, ]
+  par_rate <- par / as.numeric(par%*% rep(1, ncol(par)))   #潜在変数の割当確率
+  
+  #多項分布からトピックノードを生成
+  Zi12 <- matrix(0, nrow=f, ncol=k12[j])
+  Zi12[index_node, ] <- rmnom(length(index_node), 1, par_rate)  
+  Zi12_T_list[[j]] <- t(Zi12)
+  z12_vec[, j] <- as.numeric(Zi12 %*% 1:k12[j])
+}
+
+
+
