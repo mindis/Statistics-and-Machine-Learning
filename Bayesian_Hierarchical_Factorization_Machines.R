6a8145ccc48f5b295654b3ec8a335e41fc9d2d7b
you-chan2 <verticalbird2007@yahoo.co.jp>
2018-08-06 08:36:21 +0900

    【統計モデル】Hierarchical Factorization Machines

---
+++ Bayesian_Hierarchical_Factorization_Machines.R
@@ -0,0 +1,64 @@
+#####Bayesian Hierarchical Factorization Machines#####
+library(MASS)
+library(matrixStats)
+library(data.table)
+library(FAdist)
+library(bayesm)
+library(extraDistr)
+library(condMVNorm)
+library(actuar)
+library(gtools)
+library(caret)
+library(reshape2)
+library(dplyr)
+library(ggplot2)
+library(lattice)
+
+#set.seed(78594)
+
+####任意の分散共分散行列を作成させる関数####
+##多変量正規分布からの乱数を発生させる
+#任意の相関行列を作る関数を定義
+corrM <- function(col, lower, upper, eigen_lower, eigen_upper){
+  diag(1, col, col)
+  
+  rho <- matrix(runif(col^2, lower, upper), col, col)
+  rho[upper.tri(rho)] <- 0
+  Sigma <- rho + t(rho)
+  diag(Sigma) <- 1
+  (X.Sigma <- eigen(Sigma))
+  (Lambda <- diag(X.Sigma$values))
+  P <- X.Sigma$vector
+  
+  #新しい相関行列の定義と対角成分を1にする
+  (Lambda.modified <- ifelse(Lambda < 0, runif(1, eigen_lower, eigen_upper), Lambda))
+  x.modified <- P %*% Lambda.modified %*% t(P)
+  normalization.factor <- matrix(diag(x.modified),nrow = nrow(x.modified),ncol=1)^0.5
+  Sigma <- x.modified <- x.modified / (normalization.factor %*% t(normalization.factor))
+  diag(Sigma) <- 1
+  round(Sigma, digits=3)
+  return(Sigma)
+}
+
+##相関行列から分散共分散行列を作成する関数を定義
+covmatrix <- function(col, corM, lower, upper){
+  m <- abs(runif(col, lower, upper))
+  c <- matrix(0, col, col)
+  for(i in 1:col){
+    for(j in 1:col){
+      c[i, j] <- sqrt(m[i]) * sqrt(m[j])
+    }
+  }
+  diag(c) <- m
+  cc <- c * corM
+  #固有値分解で強制的に正定値行列に修正する
+  UDU <- eigen(cc)
+  val <- UDU$values
+  vec <- UDU$vectors
+  D <- ifelse(val < 0, val + abs(val) + 0.00001, val)
+  covM <- vec %*% diag(D) %*% t(vec)
+  data <- list(covM, cc,  m)
+  names(data) <- c("covariance", "cc", "mu")
+  return(data)
+}
+
