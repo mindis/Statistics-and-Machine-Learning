3a212d736816ae3621340b122b6aacb7b8fa10d3
you-chan2 <verticalbird2007@yahoo.co.jp>
2018-08-09 08:53:23 +0900

    【統計モデル】Hierarchical Factorization Machines

--- Bayesian_Hierarchical_Factorization_Machines.R
+++ Bayesian_Hierarchical_Factorization_Machines.R
@@ -1,6 +1,7 @@
 #####Bayesian Hierarchical Factorization Machines#####
 library(MASS)
 library(matrixStats)
+library(Matrix)
 library(data.table)
 library(FAdist)
 library(bayesm)
@@ -61,4 +62,102 @@ covmatrix <- function(col, corM, lower, upper){
   names(data) <- c("covariance", "cc", "mu")
   return(data)
 }
+
+####データの発生####
+##データの設定
+s <- 5   #基底数
+hh <- 5000   #ユーザー数
+item <- 2000   #アイテム数
+N0 <- hh*item
+
+#IDを設定
+user_id0 <- rep(1:hh, rep(item, hh))
+item_id0 <- rep(1:item, hh)
+
+##欠損ベクトルを生成
+#欠損確率を生成
+user_prob <- rbeta(hh, 8.5, 10.0)   #ユーザ-購買確率
+item_prob <- rbeta(item, 6.5, 8.0)   #アイテム購買確率
+prob <- user_prob[user_id0]*item_prob[item_id0]
+
+#ベルヌーイ分布から欠損ベクトルを生成
+z_vec <- rbinom(N0, 1, prob)
+N <- sum(z_vec)
+
+#欠損ベクトルからidを再構成
+user_id <- user_id0[z_vec==1]
+item_id <- item_id0[z_vec==1]
+rm(user_id0); rm(item_id0); rm(z_vec); rm(prob)
+gc(); gc()
+
+#購買数をカウント
+freq <- plyr::count(user_id); user_freq <- freq$freq[order(freq$x)]
+freq <- plyr::count(item_id); item_freq <- freq$freq[order(freq$x)]
+hist(user_freq, col="grey", breaks=25, main="ユーザーごとの購買数", xlab="購買数")
+hist(item_freq, col="grey", breaks=25, main="アイテムごとの購買数", xlab="購買数")
+
+
+##説明変数の生成
+#モデルの説明変数を生成
+k1 <- 4; k2 <- 5; k3 <- 5
+k <- k1 + k2 + k3
+x1 <- matrix(0, nrow=N, ncol=k1)
+x2 <- matrix(0, nrow=N, ncol=k2)
+for(j in 1:k1){
+  par <- runif(2, 1.0, 2.5)
+  x1[, j] <- rbeta(N, par[1], par[2])
+}
+for(j in 1:k2){
+  pr <- runif(1, 0.25, 0.55)
+  x2[, j] <- rbinom(N, 1, pr)
+}
+x3 <- rmnom(N, 1, runif(k3, 0.2, 1.25)); x3 <- x3[, -which.min(colSums(x3))]
+x <- cbind(1, x1, x2, x3)   #データを結合
+
+#ユーザーの説明変数を生成
+k1 <- 3; k2 <- 3; k3 <- 4
+u1 <- matrix(0, nrow=hh, ncol=k1)
+u2 <- matrix(0, nrow=hh, ncol=k2)
+for(j in 1:k1){
+  par <- runif(2, 1.0, 2.5)
+  u1[, j] <- rbeta(hh, par[1], par[2])
+}
+for(j in 1:k2){
+  pr <- runif(1, 0.25, 0.55)
+  u2[, j] <- rbinom(hh, 1, pr)
+}
+u3 <- rmnom(hh, 1, runif(k3, 0.2, 1.25)); u3 <- u3[, -which.min(colSums(u3))]
+u <- cbind(1, u1, u2, u3)   #データを結合
+
+
+#アイテムの説明変数を生成
+k1 <- 3; k2 <- 2; k3 <- 4
+v1 <- matrix(0, nrow=item, ncol=k1)
+v2 <- matrix(0, nrow=item, ncol=k2)
+for(j in 1:k1){
+  par <- runif(2, 1.0, 2.5)
+  v1[, j] <- rbeta(item, par[1], par[2])
+}
+for(j in 1:k2){
+  pr <- runif(1, 0.25, 0.55)
+  v2[, j] <- rbinom(item, 1, pr)
+}
+v3 <- rmnom(item, 1, runif(k3, 0.2, 1.25)); v3 <- v3[, -which.min(colSums(v3))]
+v <- cbind(1, v1, v2, v3)   #データを結合
+
+
+##交差項を設定
+#交差項のインデックスを作成
+v_matrix <- matrix(0, nrow=k-1, ncol=k-1)
+v_matrix[upper.tri(v_matrix)] <- 1
+v_list <- list()
+for(j in 1:(k-1)){
+  index <- which(v_matrix[j, ]==1)
+  if(length(index) > 0){
+    v_list[[j]] <- cbind(j1=j, j2=which(v_matrix[j, ]==1))
+  }
+}
+v_index <- do.call(rbind, v_list)
+
+
 
