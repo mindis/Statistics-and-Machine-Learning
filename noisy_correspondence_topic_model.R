24413c2999fbda7fce26f953749d977bc812ee91
you-chan2 <verticalbird2007@yahoo.co.jp>
2018-10-01 08:50:23 +0900

    【統計モデル】ノイズあり対応トピックモデル

--- noisy_correspondence_topic_model.R
+++ noisy_correspondence_topic_model.R
@@ -1,9 +1,11 @@
 #####ノイズあり対応トピックモデル#####
+options(warn=0)
 library(MASS)
 library(lda)
 library(RMeCab)
 library(matrixStats)
 library(Matrix)
+library(data.table)
 library(bayesm)
 library(HMM)
 library(extraDistr)
@@ -12,20 +14,21 @@ library(dplyr)
 library(plyr)
 library(ggplot2)
 
-#set.seed(8079)
+#set.seed(2506787)
 
 ####データの生成####
 #set.seed(423943)
 #文書データの設定
 k <- 15   #トピック数
-d <- 2500   #文書数
-v <- 1000   #語彙数
-w <- rpois(d, rgamma(d, 75, 0.5))   #1文書あたりの単語数
+vec_k <- rep(1, k)
+d <- 5000   #文書数
+v <- 1200   #語彙数
+w <- rpois(d, rgamma(d, 40, 0.2))   #1文書あたりの単語数
 f <- sum(w)   #総単語数
-a1 <- 100   #トピックに関係のあるタグ数
-a2 <- 50   #トピックに関係のないタグ数
+a1 <- 200   #トピックに関係のあるタグ数
+a2 <- 100   #トピックに関係のないタグ数
 a <- a1 + a2   #補助変数数
-x <- rtpois(d, 30, 2, Inf)
+x <- rtpois(d, 50, 2, Inf)
 f1 <- sum(w)
 f2 <- sum(x)
 
@@ -34,14 +37,21 @@ w_id <- rep(1:d, w)
 a_id <- rep(1:d, x)
 
 #パラメータの設定
+#トピック分布を設定
 alpha0 <- rep(0.15, k)   #文書のディレクリ事前分布のパラメータ
-alpha1 <- rep(0.15, v)   #単語のディレクリ事前分布のパラメータ
-alpha2 <- c(rep(0.2, a1), rep(0.0001, a2))   #トピックに関係のあるタグのディクレリ事前分布のパラメータ
-alpha3 <- c(rep(0.0001, a1), rep(1.25, a2))   #トピックに関係のないタグのディクレリ事前分布のパラメータ
 beta0 <- rbeta(sum(x), 0.55, 0.175)
 
+#単語分布を設定
+index <- apply(rmnom(v, f, rep(1, k)), 1, which.max)
+alpha1 <- rep(0.04, v)
+alpha2 <- c(rep(0.1, a1), rep(0.0001, a2))   #トピックに関係のあるタグのディクレリ事前分布のパラメータ
+alpha3 <- c(rep(0.0001, a1), rep(1.0, a2))   #トピックに関係のないタグのディクレリ事前分布のパラメータ
+
+
 ##モデルに基づき単語を生成
-for(rp in 1:1000){
+rp <- 0
+repeat {
+  rp <- rp + 1
   print(rp)
   
   #ディレクリ分布からパラメータを生成
@@ -51,6 +61,13 @@ for(rp in 1:1000){
   omegat <- omega <- extraDistr::rdirichlet(k, alpha2)   #補助情報のトピック分布をディクレリ乱数から生成
   gammat <- gamma <- extraDistr::rdirichlet(1, alpha3)   #トピックに関係のないタグ
   omega0 <- rbind(omega, gamma)   #単語分布の結合
+  
+  #単語出現確率が低いトピックを入れ替える
+  index <- which(colMaxs(phi) < (k*10)/f)
+  for(j in 1:length(index)){
+    phi[as.numeric(rmnom(1, 1, extraDistr::rdirichlet(1, alpha0)) %*% 1:k), index[j]] <- (k*10)/f
+  }
+  phit <- phi
   
   ##多項分布からトピックおよび単語データを生成
   WX <- matrix(0, nrow=d, ncol=v)
@@ -63,7 +80,7 @@ for(rp in 1:1000){
   
   #文書ごとにトピックと単語を逐次生成
   for(i in 1:d){
-    
+
     #文書のトピック分布を生成
     z1 <- rmnom(w[i], 1, theta[i, ])   #文書のトピック分布を生成
     z1_vec <- as.numeric(z1 %*% 1:k)
@@ -112,73 +129,51 @@ storage.mode(AX) <- "integer"
 r0 <- c(mean(Z0), 1-mean(Z0))
 
 #単語ベクトルを行列化
-aux_table <- table(1:f2, ad)
-aux_data <- matrix(as.numeric(aux_table), nrow=f2, ncol=a)
-storage.mode(aux_data) <- "integer"
-rm(aux_table)
+word_data <- sparseMatrix(1:f1, wd, x=rep(1, f1), dims=c(f1, v))
+aux_data <- sparseMatrix(1:f2, ad, x=rep(1, f2), dims=c(f2, a))
 
-
-####トピックモデル推定のためのデータと関数の準備####
-##インデックスを作成
-doc1_list <- list()
-doc1_vec <- list()
-word_list <- list()
-word_vec <- list()
-doc2_list <- list()
-doc2_vec <- list()
-aux_list <- list()
-aux_vec <- list()
-
-for(i in 1:d){
-  doc1_list[[i]] <- which(w_id==i)
-  doc1_vec[[i]] <- rep(1, length(doc1_list[[i]]))
-  doc2_list[[i]] <- which(a_id==i)
-  doc2_vec[[i]] <- rep(1, length(doc2_list[[i]]))
-}
-for(j in 1:v){
-  word_list[[j]] <- which(wd==j)
-  word_vec[[j]] <- rep(1, length(word_list[[j]]))
-}
-for(j in 1:a){
-  aux_list[[j]] <- which(ad==j)
-  aux_vec[[j]] <- rep(1, length(aux_list[[j]]))
-}
-gc(); gc()
 
 ####マルコフ連鎖モンテカルロ法で対応トピックモデルを推定####
 ##単語ごとに尤度と負担率を計算する関数
-burden_fr <- function(theta, phi, wd, w, k){
+burden_fr <- function(theta, phi, wd, w, k, vec_k){
   #負担係数を計算
   Bur <- theta[w, ] * t(phi)[wd, ]   #尤度
-  Br <- Bur / rowSums(Bur)   #負担率
-  r <- colSums(Br) / sum(Br)   #混合率
-  bval <- list(Br=Br, Bur=Bur, r=r)
+  Br <- Bur / as.numeric(Bur %*% vec_k)   #負担率
+  bval <- list(Br=Br, Bur=Bur)
   return(bval)
 }
 
 
 ##アルゴリズムの設定
-R <- 5000   #サンプリング回数
+R <- 3000   #サンプリング回数
 keep <- 2   #2回に1回の割合でサンプリング結果を格納
 disp <- 10
 iter <- 0
 burnin <- 1000/keep
 
+##インデックスを設定
+d_dt <- sparseMatrix(w_id, 1:f1, x=rep(1, f1), dims=c(d, f1))
+a_dt <- sparseMatrix(a_id, 1:f2, x=rep(1, f2), dims=c(d, f2))
+word_dt <- t(word_data)
+aux_dt <- t(aux_data)
+
 ##事前分布の設定
 #ハイパーパラメータの事前分布
-alpha01 <- 1
-alpha02 <- 1
-beta01 <- 0.25
-beta02 <- 0.25
-gamma01 <- 0.01
-gamma02 <- 0.01
+alpha01 <- 0.25
+alpha02 <- 0.25
+beta01 <- 0.01
+beta02 <- 0.01
+s0 <- 0.01
+v0 <- 0.01
+
+##パラメータの真値
+theta <- thetat
+phi <- phit
+omega <- omegat
+gamma <- gammat
+r <- 0.5
 
 ##パラメータの初期値
-#tfidfで初期値を設定
-tf <- AX / rowSums(AX)
-idf1 <- log(nrow(AX) / colSums(AX > 0))
-idf2 <- log(nrow(AX) / colSums(AX==0))
-
 theta <- extraDistr::rdirichlet(d, rep(1, k))   #文書トピックのパラメータの初期値
 phi <- extraDistr::rdirichlet(k, rep(2.0, v))   #単語トピックのパラメータの初期値
 omega <- extraDistr::rdirichlet(k, rep(10, a))   #タグのトピックのパラメータの初期値
@@ -190,6 +185,7 @@ THETA <- array(0, dim=c(d, k, R/keep))
 PHI <- array(0, dim=c(k, v, R/keep))
 OMEGA <- array(0, dim=c(k, a, R/keep))
 GAMMA <- matrix(0, nrow=R/keep, ncol=a)
+LAMBDA <- rep(0, R/keep)
 Z_SEG <- rep(0, f2)
 W_SEG <- matrix(0, nrow=f1, ncol=k)
 A_SEG <- matrix(0, nrow=f2, ncol=k+1)
@@ -209,72 +205,64 @@ for(rp in 1:R){
   
   ##単語トピックをサンプリング
   #単語ごとにトピックの出現確率を計算
-  word_par <- burden_fr(theta, phi, wd, w_id, k)
+  word_par <- burden_fr(theta, phi, wd, w_id, k, vec_k)
   word_rate <- word_par$Br
   
   #多項分布から単語トピックをサンプリング
   Zi1 <- rmnom(f1, 1, word_rate)   
-  Zi1_T <- t(Zi1)
   z1_vec <- as.numeric(Zi1 %*% 1:k)
   
   
   ##単語トピックのパラメータを更新
   #ディクレリ分布からthetaをサンプリング
-  wsum0 <- matrix(0, nrow=d, ncol=k)
-  for(i in 1:d){
-    wsum0[i, ] <- Zi1_T[, doc1_list[[i]]] %*% doc1_vec[[i]]
-  }
+  wsum0 <- as.matrix(d_dt %*% Zi1)
   wsum <- wsum0 + alpha01   #ディクレリ分布のパラメータ
-  theta <- extraDistr::rdirichlet(d, wsum)   #ディクレリ分布からthetaをサンプリング
+  theta <- extraDistr::rdirichlet(d, wsum)   #パラメータをサンプリング
   
   #ディクレリ分布からphiをサンプリング
-  vf0 <- matrix(0, nrow=k, ncol=v)
-  for(j in 1:v){
-    vf0[, j] <- Zi1_T[, word_list[[j]]] %*% word_vec[[j]]
-  }
-  vf <- vf0 + beta01   #ディクレリ分布のパラメータ
-  phi <- extraDistr::rdirichlet(k, vf)   #ディクレリ分布からphiをサンプリング
+  vf <- as.matrix(t(word_dt %*% Zi1)) + beta01   #ディクレリ分布のパラメータ
+  phi <- extraDistr::rdirichlet(k, vf)   #パラメータをサンプリング
   
   
   ##タグが文書のトピックと関連があるかどうかを決定
-  #生成させた単語トピックからトピック抽出確率を計算
+  #生成させた単語トピックから補助情報のトピック分布を設定
   theta_aux <- wsum0 / w
   
   #ベルヌーイ分布の確率を計算
-  tau01 <- r * rowSums(theta_aux[a_id, ] * t(omega)[ad, ])   #補助変数トピックの尤度
-  tau02 <- (1-r) * gamma[ad]   #ノイズ補助変数の尤度
+  aux_par <- theta_aux[a_id, ] * t(omega)[ad, ]   #補助情報トピックの尤度
+  aux_sums <- as.numeric(aux_par %*% vec_k)
+  tau01 <- r * aux_sums   #補助情報トピックの割当尤度
+  tau02 <- (1-r) * gamma[ad]   #ノイズ補助変数の割当尤度
   tau <- tau01 / (tau01 + tau02)  
   
   #ベルヌーイ分布よりノイズの潜在変数を生成
   z <- rbinom(f2, 1, tau)
-  r <- mean(z)   #混合率
-
+  
+  #ベータ分布から混合率をサンプリング
+  n <- sum(z)
+  s1 <- n + s0
+  v1 <- f2 - n + v0
+  r <- rbeta(1, s1, v1)   #混合率をサンプリング
   
   ##補助情報トピックをサンプリング
   #z=1の場合、タグごとにトピックの出現率を計算
   index_z <- which(z==1)   #z=1のみ抽出
-  aux_par <- burden_fr(theta_aux, omega, ad[index_z], a_id[index_z], k)
-  aux_rate <- aux_par$Br
+  aux_rate <- aux_par[index_z, ] / aux_sums[index_z]
   
   #多項分布から補助情報トピックをサンプリング
   Zi2 <- matrix(0, nrow=f2, ncol=k+1)
-  Zi2[index_z, -(k+1)] <- rmnom(length(index_z), 1, aux_rate)   #多項分布からトピックをサンプリング
+  Zi2[index_z, 1:k] <- rmnom(n, 1, aux_rate)   #多項分布からトピックをサンプリング
   Zi2[-index_z, k+1] <- 1
-  Zi2_T <- t(Zi2[, -(k+1)])
   z2_vec <- as.numeric(Zi2 %*% 1:(k+1))
   
   
   ##タグトピックのパラメータを更新
-  af0 <- matrix(0, nrow=k, ncol=a)
-  for(j in 1:a){
-    af0[, j] <- Zi2_T[, aux_list[[j]]] %*% aux_vec[[j]]
-  }
-  af <- af0 + gamma01   #ディクレリ分布のパラメータ
-  omega <- extraDistr::rdirichlet(k, af)   #ディクレリ分布からomegaをサンプリング
+  af <- as.matrix(t(aux_dt %*% Zi2[, 1:k])) + beta02   #ディクレリ分布のパラメータ
+  omega <- extraDistr::rdirichlet(k, af)   #パラメータをサンプリング
   
   ##内容に関係のないタグのパラメータの更新
-  nf <- colSums(aux_data[-index_z, ]) + gamma02   #ディクレリ分布のパラメータ
-  gamma <- as.numeric(extraDistr::rdirichlet(1, nf))   #ディクレリ分布からgammaをサンプリング
+  nf <- as.numeric(aux_dt %*% Zi2[, k+1]) + beta02   #ディクレリ分布のパラメータ
+  gamma <- as.numeric(extraDistr::rdirichlet(1, nf))   #パラメータをサンプリング
   
   
   ##パラメータの格納とサンプリング結果の表示
@@ -286,6 +274,7 @@ for(rp in 1:R){
     PHI[, , mkeep] <- phi
     OMEGA[, , mkeep] <- omega
     GAMMA[mkeep, ] <- gamma
+    LAMBDA[mkeep] <- r
     
     #トピック割当はバーンイン期間を超えたら格納する
     if(rp%%keep==0 & rp >= burnin){
@@ -293,27 +282,26 @@ for(rp in 1:R){
       A_SEG <- A_SEG + Zi2
       Z_SEG <- Z_SEG + z
     }
+  }
+  
+  if(rp%%disp==0){
+    #対数尤度の計算
+    LL1 <- sum(log(word_par$Bur %*% vec_k))
+    LL2 <- sum(log(aux_par[index_z, ] %*% vec_k)) + sum(log(gamma[ad[-index_z]]))
+    LL <- LL1 + LL2
     
-    if(rp%%disp==0){
-      
-      #対数尤度の計算
-      LL1 <- sum(log(rowSums(word_par$Bur)))
-      LL2 <- sum(log(rowSums(aux_par$Bur)))
-      LL3 <- sum(log(gamma[ad][-index_z]))
-      LL <- LL1 + LL2 + LL3
-      
-      #サンプリング結果を確認
-      print(rp)
-      print(c(LL, LLst, LL1, LLst1, LL2+LL3, LLst2))
-      print(round(c(r, r0[1]), 3))
-      print(round(cbind(omega[, 96:105], omegat[, 96:105]), 3))
-      print(round(rbind(gamma[91:110], gammat[91:110]), 3))
-    }
+    #サンプリング結果を確認
+    print(rp)
+    print(c(LL, LLst, LL1, LL2, LLst1, LLst2))
+    print(round(c(r, r0[1]), 3))
+    print(round(cbind(omega[, (a1-4):(a1+5)], omegat[, (a1-4):(a1+5)]), 3))
+    print(round(rbind(gamma[(a1-9):(a1+10)], gammat[(a1-9):(a1+10)]), 3))
   }
 }
 
+
 ####サンプリング結果の可視化と要約####
-burnin <- 2000   #バーンイン期間
+burnin <- 500   #バーンイン期間
 
 ##サンプリング結果の可視化
 #文書のトピック分布のサンプリング結果
