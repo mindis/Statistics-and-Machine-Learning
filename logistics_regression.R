4977e3b0149d303bd3501dcbcee27552956a06c9
you-chan2 <verticalbird2007@yahoo.co.jp>
2018-10-12 08:18:20 +0900

    【統計モデル】ロジスティック回帰モデル

--- logistics_regression.R
+++ logistics_regression.R
@@ -3,73 +3,94 @@ library(knitr)
 library(caret)
 library(reshape2)
 library(plyr)
+library(matrixStats)
+library(extraDistr)
 library(ggplot2)
 library(lattice)
 
 ####データの発生####
 #set.seed(4543)
-#説明変数とパラメータの設定
-col <- 10   #パラメータ数
-x <- rnorm(5000, 0, 3)
-alpha <- 0.5
-beta1 <- rnorm(10, 0, 0.7)
-(beta <- c(alpha, beta1))
-Xm <- matrix(x, nrow=length(x)/col, ncol=col, byrow=T)  
-X <- as.data.frame(Xm)
-X1 <- cbind(1, Xm)
-n <- nrow(X)   #サンプル数
+##説明変数の発生
+#データの設定
+hh <- 250000   #レコード数
+k1 <- 5; k2 <- 10; k3 <- 8   #変数数
+k <- k1 + k2 + k3
 
-#選択確率の計算
-p <- exp(X1 %*% as.vector(beta)) / (1 + exp(X1 %*% as.vector(beta)))
-#p <- plogis(alpha + beta1 * x)   #これでもOK
-mean(p)
+#変数ごとにデータを発生
+x1 <- matrix(runif(hh*k1, 0, 1), nrow=hh, ncol=k1)
+x2 <- matrix(0, nrow=hh, ncol=k2)
+for(j in 1:k2){
+  pr <- runif(1, 0.25, 0.55)
+  x2[, j] <- rbinom(hh, 1, pr)
+}
+x3 <- rmnom(hh, 1, runif(k3, 0.2, 1.25)); x3 <- x3[, -which.min(colSums(x3))]
+x <- cbind(1, x1, x2, x3)   #データを結合
 
-#選択結果を発生させる
-choice <- rbinom(n, 1, p)
-mean(choice)
+##応答変数の発生
+#パラメータの設定
+beta <- betat <- c(-0.75, rnorm(k-1, 0, 1.0))
 
-#データを結合
-Xy <- cbind(choice, X) 
-Xy <- as.data.frame(Xy)
-round(head(Xy, 20), 4)
+#ロジットと選択確率を設定
+logit <- as.numeric(x %*% beta)
+prob <- exp(logit) / (1 + exp(logit))
 
-####ロジスティック回帰モデルを推定####
-##対数尤度の設定
-fr <- function(b, x, y){
-  #パラメータの設定
-  alpha <- b[1]
-  beta <- b[2:11]
+#ベルヌーイ分布から応答変数を発生
+y <- rbinom(hh, 1, prob)
+hist(prob, breaks=50, col="grey", main="確率の分布", xlab="確率")
+mean(prob[y==1])
+mean(prob[y==0])
+mean(prob)
+
+
+####最尤法でロジスティック回帰モデルを推定####
+##対数尤度関数を設定
+fr <- function(beta, x, y){
+  #ロジットと確率を設定
+  logit_exp <- exp(as.numeric(x %*% beta))
+  prob <- logit_exp / (1 + logit_exp)
   
-  #尤度を定義して合計する
-  Xb <- alpha + as.matrix(X) %*% as.vector(beta) 
-  p <- exp(Xb) / (1 + exp(Xb))
-  LLS <- y*log(p) + (1-y)*log(1-p)  
-  LL <- sum(LLS)
+  #対数尤度の和
+  LL <- sum(y*log(prob) + (1-y)*log(1-prob))
   return(LL)
 }
 
-##対数尤度を最大化する
-b0 <- c(rep(0, 11))   #初期パラメータの設定
-res <- optim(b0, fr, gr=NULL, x=X, y=choice, method="BFGS", hessian=TRUE, control=list(fnscale=-1))
+##対数尤度の微分関数を設定
+dlogit <- function(beta, x, y){
+  #ロジットと確率を設定
+  logit_exp <- exp(as.numeric(x %*% beta))
+  prob <- logit_exp / (1 + logit_exp)
+  
+  #対数尤度の勾配ベクトル
+  LLd <- colSums(y*x - x*prob)
+  return(LLd)
+}
 
-#結果
-(b <- res$par)   #推定されたパラメータ
-beta   #真のパラメータ
-(tval <- b/sqrt(-diag(solve(res$hessian))))   #t値
-(AIC <- -2*res$value + 2*length(res$par))   #AIC
-(BIC <- -2*res$value + log(n)*length(b))   #BIC
-
-#適合度
-prob <- round(exp(as.matrix(X1) %*% as.vector(b)) / (1 + exp(as.matrix(X1) %*% as.vector(b))), 3)   #推定された確率
-cbind(round(p, 3), prob , round(p-prob, 3))   #真の確率との誤差
-rbind(beta, b, beta-b)   #パラメータの誤差
+##準ニュートン法で対数尤度を最大化する
+b0 <- c(rep(0, k))   #初期パラメータの設定
+res1 <- optim(b0, fr, gr=dlogit, x, y, method="BFGS", hessian=TRUE, control=list(fnscale=-1, trace=TRUE))
 
 #関数を使うなら
-res2 <- glm(choice ~ V1 + V2 + V3 + V4 +V5 + V6 + V7 + V8 + V9 + V10, data = Xy, family=binomial(link=logit))
+z <- x[, -1]
+res2 <- glm(y ~ z, family=binomial(link=logit))
 summary(res2)
-glmb <- coef(res2)
+beta_glm <- as.numeric(coef(res2))
 
-error <- rbind(b, glmb, beta, b-beta, glmb-beta)   #パラメータの誤差
-rownames(error) <- c("beta", "glmb", "btrue", "beta-btrue", "glmb-btrue")
-kable(round(error, 2))
+##結果を表示
+beta <- res1$par   #推定されたパラメータ
+round(rbind(beta, beta_glm, betat), 3)   #真のパラメータとの比較「
+(tval <- beta / sqrt(-diag(solve(res1$hessian))))   #t値
+(AIC <- -2*res1$value + 2*length(res1$par))   #AIC
+(BIC <- -2*res1$value + log(hh)*length(beta))   #BIC
+
+##適合度
+#推定された確率
+logit1 <- as.numeric(x %*% beta)
+prob1 <- exp(logit) / (1 + exp(logit))   #推定された確率
+
+#真の確率
+logit2 <- as.numeric(x %*% betat)
+prob2 <- exp(logit) / (1 + exp(logit))   #真の確率
+
+#比較を行う
+round(cbind(prob1, prob2), 3)
 
